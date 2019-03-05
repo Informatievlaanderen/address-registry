f7be484d7bca0ef21f18df959fdc82fbfc7a0703
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-03-05 14:42:04 +0100

    fix: correct typo municipality event

--- src/AddressRegistry.Projections.Syndication/Municipality/MunicipalityEvent.cs
+++ src/AddressRegistry.Projections.Syndication/Municipality/MunicipalityEvent.cs
@@ -11,7 +11,7 @@ namespace AddressRegistry.Projections.Syndication.Municipality
         MunicipalityNisCodeWasCorrected,
         MunicipalityOfficialLanguageWasAdded,
         MunicipalityOfficialLanguageWasRemoved,
-        MunicipalityFacilitiesLanuageWasAdded,
-        MunicipalityFacilitiesLanuageWasRemoved,
+        MunicipalityFacilitiesLanguageWasAdded,
+        MunicipalityFacilitiesLanguageWasRemoved,
     }
 }

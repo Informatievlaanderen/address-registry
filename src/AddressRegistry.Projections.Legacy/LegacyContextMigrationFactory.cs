87974d84fd9e2ab9f5fdcd80a7163c9dacc7605d
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-04-26 09:42:30 +0200

    feat: add projector + clean up projection libs

---
+++ src/AddressRegistry.Projections.Legacy/LegacyContextMigrationFactory.cs
@@ -0,0 +1,23 @@
+namespace AddressRegistry.Projections.Legacy
+{
+    using Be.Vlaanderen.Basisregisters.ProjectionHandling.Runner;
+    using Infrastructure;
+    using Microsoft.EntityFrameworkCore;
+
+    public class LegacyContextMigrationFactory : RunnerDbContextMigrationFactory<LegacyContext>
+    {
+        public LegacyContextMigrationFactory()
+            : base("LegacyProjectionsAdmin", HistoryConfiguration)
+        { }
+
+        private static MigrationHistoryConfiguration HistoryConfiguration =>
+            new MigrationHistoryConfiguration
+            {
+                Schema = Schema.Legacy,
+                Table = MigrationTables.Legacy
+            };
+
+        protected override LegacyContext CreateContext(DbContextOptions<LegacyContext> migrationContextOptions)
+            => new LegacyContext(migrationContextOptions);
+    }
+}
---
+++ src/EF.MigrationsHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Basisregisters Vlaanderen")]
+[assembly: AssemblyDescription("Microsoft SQL Server configuration provider.")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
+[assembly: AssemblyCompany("Vlaamse overheid")]
+
+[assembly: Guid("476f928e-6830-462f-a8a0-04bc3d8de2bb")]
+
+[assembly: NeutralResourcesLanguage("en")]

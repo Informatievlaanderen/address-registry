121c1ddbd46b39df921560be3592e503962bc986
David Cumps <david@cumps.be>
2019-07-08 15:26:42 +0200

    fix: use correct docker ids

--- src/AddressRegistry.Projector/init.sh
+++ src/AddressRegistry.Projector/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CONTAINERID=$(curl -s http://169.254.170.2/v2/metadata | jq -r ".Containers[] | select(.Labels[\"com.amazonaws.ecs.container-name\"] | contains(\"basisregisters-\") and contains(\"-import\")) | .DockerId")
+CONTAINERID=$(curl -s http://169.254.170.2/v2/metadata | jq -r ".Containers[] | select(.Labels[\"com.amazonaws.ecs.container-name\"] | contains(\"basisregisters-\") and contains(\"-projections\")) | .DockerId")
 
 sed -i "s/REPLACE_CONTAINERID/$CONTAINERID/g" appsettings.json
 

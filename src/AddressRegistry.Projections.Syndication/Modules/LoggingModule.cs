7ee877678b66ac2cf155fc541285b1adb746d58c
David Cumps <david@cumps.be>
2019-07-17 13:52:29 +0200

    fix: do not hardcode logging to console

--- src/AddressRegistry.Projections.Syndication/Modules/LoggingModule.cs
+++ src/AddressRegistry.Projections.Syndication/Modules/LoggingModule.cs
@@ -24,7 +24,6 @@ namespace AddressRegistry.Projections.Syndication.Modules
                     fileSizeLimitBytes: 104857600,
                     rollOnFileSizeLimit: true,
                     rollingInterval: RollingInterval.Day)
-                .WriteTo.Console()
                 .Enrich.FromLogContext()
                 .Enrich.WithMachineName()
                 .Enrich.WithThreadId()

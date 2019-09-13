c7dc77a426f0349e4c7b88d8247406668279c02e
David Cumps <david@cumps.be>
2019-09-13 23:10:31 +0200

    fix: add municipality bosa syndication index

---
+++ src/AddressRegistry.Projections.Syndication/Migrations/20190913211001_AddMunicipalityBosaFlemishRegionIndex.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AddressRegistry.Projections.Syndication.Migrations
+{
+    public partial class AddMunicipalityBosaFlemishRegionIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_MunicipalityBosa_IsFlemishRegion",
+                schema: "AddressRegistrySyndication",
+                table: "MunicipalityBosa",
+                column: "IsFlemishRegion");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_MunicipalityBosa_IsFlemishRegion",
+                schema: "AddressRegistrySyndication",
+                table: "MunicipalityBosa");
+        }
+    }
+}

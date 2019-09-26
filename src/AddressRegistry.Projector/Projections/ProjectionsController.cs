581b07a664c7cf3ca8d6c89c813310ba91dc0f31
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-09-26 07:50:48 +0200

    feat: upgrade projector and resume projections on startup

--- src/AddressRegistry.Projector/Projections/ProjectionsController.cs
+++ src/AddressRegistry.Projector/Projections/ProjectionsController.cs
@@ -1,16 +1,23 @@
 namespace AddressRegistry.Projector.Projections
 {
+    using AddressRegistry.Infrastructure;
     using Be.Vlaanderen.Basisregisters.Api;
     using Be.Vlaanderen.Basisregisters.Projector.ConnectedProjections;
     using Be.Vlaanderen.Basisregisters.Projector.Controllers;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
 
     [ApiVersion("1.0")]
     [ApiRoute("projections")]
     public class ProjectionsController : DefaultProjectorController
     {
-        public ProjectionsController(IConnectedProjectionsManager connectedProjectionsManager)
+        public ProjectionsController(
+            IConnectedProjectionsManager connectedProjectionsManager,
+            IConfiguration configuration)
             : base(connectedProjectionsManager)
-        { }
+        {
+            RegisterConnectionString(Schema.Legacy, configuration.GetConnectionString("LegacyProjections"));
+            RegisterConnectionString(Schema.Extract, configuration.GetConnectionString("ExtractProjections"));
+        }
     }
 }

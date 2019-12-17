f2a50dad6fe4cb533eabafc88094a9fb103166f0
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-12-17 11:23:09 +0100

    perf: sync add seperate index on parcel

---
+++ src/AddressRegistry.Projections.Syndication/Migrations/20191217102212_AddParcelIndex.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AddressRegistry.Projections.Syndication.Migrations
+{
+    public partial class AddParcelIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ParcelAddressMatchLatestItemSyndication_ParcelId",
+                schema: "AddressRegistrySyndication",
+                table: "ParcelAddressMatchLatestItemSyndication",
+                column: "ParcelId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ParcelAddressMatchLatestItemSyndication_ParcelId",
+                schema: "AddressRegistrySyndication",
+                table: "ParcelAddressMatchLatestItemSyndication");
+        }
+    }
+}

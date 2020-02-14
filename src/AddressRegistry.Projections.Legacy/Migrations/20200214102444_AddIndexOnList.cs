53cfc8ebcc1f6634ff9b096115d2b0d6cbf2c035
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-02-14 11:25:07 +0100

    fix: add index on list

---
+++ src/AddressRegistry.Projections.Legacy/Migrations/20200214102444_AddIndexOnList.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AddressRegistry.Projections.Legacy.Migrations
+{
+    public partial class AddIndexOnList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AddressList_Complete_Removed_PersistentLocalId",
+                schema: "AddressRegistryLegacy",
+                table: "AddressList",
+                columns: new[] { "Complete", "Removed", "PersistentLocalId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AddressList_Complete_Removed_PersistentLocalId",
+                schema: "AddressRegistryLegacy",
+                table: "AddressList");
+        }
+    }
+}

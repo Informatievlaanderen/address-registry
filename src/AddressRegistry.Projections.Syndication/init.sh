2bb63f44f65cdfa8c00b53f019d6e192feeadaa5
David Cumps <david@cumps.be>
2020-03-03 13:06:38 +0100

    fix: update dockerid detection

--- src/AddressRegistry.Projections.Syndication/init.sh
+++ src/AddressRegistry.Projections.Syndication/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CONTAINERID=$(curl -s http://169.254.170.2/v2/metadata | jq -r ".Containers[] | select(.Labels[\"com.amazonaws.ecs.container-name\"] | contains(\"basisregisters-\") and contains(\"-syndication\")) | .DockerId")
+CONTAINERID=$(curl -s http://169.254.170.2/v2/metadata | jq -r ".Containers[] | select(.Labels[\"com.amazonaws.ecs.container-name\"] | startswith(\"basisregisters-\") and endswith(\"-syndication\")) | .DockerId")
 
 sed -i "s/REPLACE_CONTAINERID/$CONTAINERID/g" appsettings.json
 
--- src/AddressRegistry.Projector/init.sh
+++ src/AddressRegistry.Projector/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CONTAINERID=$(curl -s http://169.254.170.2/v2/metadata | jq -r ".Containers[] | select(.Labels[\"com.amazonaws.ecs.container-name\"] | contains(\"basisregisters-\") and contains(\"-projections\")) | .DockerId")
+CONTAINERID=$(curl -s http://169.254.170.2/v2/metadata | jq -r ".Containers[] | select(.Labels[\"com.amazonaws.ecs.container-name\"] | startswith(\"basisregisters-\") and endswith(\"-projections\")) | .DockerId")
 
 sed -i "s/REPLACE_CONTAINERID/$CONTAINERID/g" appsettings.json
 

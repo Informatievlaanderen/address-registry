4c7ebfcf8cf7a53df797c51d6f0966e51bea4cb4
David Cumps <david@cumps.be>
2020-03-03 17:10:49 +0100

    feat: upgrade netcoreapp31 and dependencies

--- src/AddressRegistry.Projector/init.sh
+++ src/AddressRegistry.Projector/init.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-CONTAINERID=$(curl -s http://169.254.170.2/v2/metadata | jq -r ".Containers[] | select(.Labels[\"com.amazonaws.ecs.container-name\"] | startswith(\"basisregisters-\") and endswith(\"-projections\")) | .DockerId")
+CT=projections
 
-curl -s http://169.254.170.2/v2/metadata
+curl -s http://169.254.170.2/v2/metadata > metadata
+cat metadata
+
+CONTAINERID=$(cat metadata | jq -r ".Containers[] | select(.Labels[\"com.amazonaws.ecs.container-name\"] | startswith(\"basisregisters-\") and endswith(\"-$CT\")) | .DockerId")
+echo CONTAINERID = $CONTAINERID
 
 sed -i "s/REPLACE_CONTAINERID/$CONTAINERID/g" appsettings.json
 

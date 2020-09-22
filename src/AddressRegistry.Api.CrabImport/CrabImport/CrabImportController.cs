37a2a75a438d9985fd1ca139a362a32742602193
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-09-22 11:55:08 +0200

    feat: add import status endpoint GRAR-1400

--- src/AddressRegistry.Api.CrabImport/CrabImport/CrabImportController.cs
+++ src/AddressRegistry.Api.CrabImport/CrabImport/CrabImportController.cs
@@ -142,6 +142,17 @@ namespace AddressRegistry.Api.CrabImport.CrabImport
 
             return Ok();
         }
+
+        [HttpGet("status/{feed}")]
+        public IActionResult GetStatus(
+            [FromServices] CrabImportContext context,
+            [FromRoute] string feed)
+            => Ok(context.StatusFor((ImportFeed)feed));
+
+        [HttpGet("status")]
+        public IActionResult GetStatus(
+            [FromServices] CrabImportContext context)
+            => Ok(context.StatusForAllFeeds());
     }
 
     public class RegisterCrabImportResponseExamples : IExamplesProvider<object>

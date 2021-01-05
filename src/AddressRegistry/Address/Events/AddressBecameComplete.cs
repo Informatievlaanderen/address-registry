3ef328ee642918ea46917b42fc27ec3acd783793
rubcap <26146350+rubcap@users.noreply.github.com>
2021-01-05 13:36:57 +0100

    docs: changed EventDescription/added EventPropertyDescriptions GRAR-1617

--- src/AddressRegistry/Address/Events/AddressBecameComplete.cs
+++ src/AddressRegistry/Address/Events/AddressBecameComplete.cs
@@ -6,10 +6,13 @@ namespace AddressRegistry.Address.Events
     using System;
 
     [EventName("AddressBecameComplete")]
-    [EventDescription("Het adres werd vervolledigd.")]
+    [EventDescription("Het adres voldoet aan het informatiemodel (wegens volledig).")]
     public class AddressBecameComplete : IHasProvenance, ISetProvenance
     {
+        [EventPropertyDescription("Interne GUID van het adres.")]
         public Guid AddressId { get; }
+        
+        [EventPropertyDescription("Metadata bij het event.")]
         public ProvenanceData Provenance { get; private set; }
 
         public AddressBecameComplete(AddressId addressId) => AddressId = addressId;

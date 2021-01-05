45fb3b55df2775ae6e454a619c93fc4af9835401
rubcap <26146350+rubcap@users.noreply.github.com>
2021-01-05 13:37:29 +0100

    docs: added EventPropertyDescriptions GRAR-1617

--- src/AddressRegistry/Address/Events/AddressHouseNumberWasCorrected.cs
+++ src/AddressRegistry/Address/Events/AddressHouseNumberWasCorrected.cs
@@ -9,8 +9,13 @@ namespace AddressRegistry.Address.Events
     [EventDescription("Het huisnummer van het adres werd gecorrigeerd.")]
     public class AddressHouseNumberWasCorrected : IHasProvenance, ISetProvenance
     {
+        [EventPropertyDescription("Interne GUID van het adres.")]
         public Guid AddressId { get; }
+        
+        [EventPropertyDescription("Huisnummer van het adres.")]
         public string HouseNumber { get; }
+        
+        [EventPropertyDescription("Metadata bij het event.")]
         public ProvenanceData Provenance { get; private set; }
 
         public AddressHouseNumberWasCorrected(

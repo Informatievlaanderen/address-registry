f88d420933390ae10521892e11cbb96a0c3b550c
rubcap <26146350+rubcap@users.noreply.github.com>
2021-01-08 15:07:55 +0100

    docs: added EventPropertyDescriptions GRAR-1728

--- src/AddressRegistry/Address/Events/Crab/AddressSubaddressStatusWasImportedFromCrab.cs
+++ src/AddressRegistry/Address/Events/Crab/AddressSubaddressStatusWasImportedFromCrab.cs
@@ -10,14 +10,31 @@ namespace AddressRegistry.Address.Events.Crab
     [EventDescription("Legacy event om tblSubadresstatus en tblSubadresstatus_hist te importeren.")]
     public class AddressSubaddressStatusWasImportedFromCrab : ICrabEvent, IHasCrabAddressStatus, IHasCrabKey<int>
     {
+        [EventPropertyDescription("CRAB-identificator van de subadresstatus.")]
         public int SubaddressStatusId { get; }
+        
+        [EventPropertyDescription("CRAB-identificator van het subadres (bus- of appartementsnummer).")]
         public int SubaddressId { get; }
+        
+        [EventPropertyDescription("Subadresstatus.")]
         public CrabAddressStatus AddressStatus { get; }
+        
+        [EventPropertyDescription("Datum waarop het object is ontstaan in werkelijkheid.")]
         public LocalDateTime? BeginDateTime { get; }
+        
+        [EventPropertyDescription("Datum waarop het object in werkelijkheid ophoudt te bestaan.")]
         public LocalDateTime? EndDateTime { get; }
+        
+        [EventPropertyDescription("Tijdstip waarop het object werd ingevoerd in de databank.")] 
         public Instant Timestamp { get; }
+        
+        [EventPropertyDescription("Operator door wie het object werd ingevoerd in de databank.")]
         public string Operator { get; }
+        
+        [EventPropertyDescription("Bewerking waarmee het object werd ingevoerd in de databank.")] 
         public CrabModification? Modification { get; }
+        
+        [EventPropertyDescription("Organisatie die het object heeft ingevoerd in de databank.")]
         public CrabOrganisation? Organisation { get; }
 
         public int Key => SubaddressStatusId;

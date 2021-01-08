01e05f0e45e443b3ab1339e8b1ba86160b1102fc
rubcap <26146350+rubcap@users.noreply.github.com>
2021-01-08 15:07:58 +0100

    docs: added EventPropertyDescriptions GRAR-1728

--- src/AddressRegistry/Address/Events/Crab/AddressSubaddressWasImportedFromCrab.cs
+++ src/AddressRegistry/Address/Events/Crab/AddressSubaddressWasImportedFromCrab.cs
@@ -10,15 +10,34 @@ namespace AddressRegistry.Address.Events.Crab
     [EventDescription("Legacy event om tblSubAdres en tblSubAdres_hist te importeren.")]
     public class AddressSubaddressWasImportedFromCrab
     {
+        [EventPropertyDescription("CRAB-identificator van het subadres (bus- of appartementsnummer).")]
         public int SubaddressId { get; }
+        
+        [EventPropertyDescription("CRAB-identificator van het huisnummer.")]
         public int HouseNumberId { get; }
+        
+        [EventPropertyDescription("Aanduiding gebruikt voor het subadres.")]
         public string BoxNumber { get; }
+        
+        [EventPropertyDescription("Aard van het subadres.")]
         public string BoxNumberType { get; }
+        
+        [EventPropertyDescription("Datum waarop het object is ontstaan in werkelijkheid.")]
         public LocalDateTime? BeginDateTime { get; }
+        
+        [EventPropertyDescription("Datum waarop het object in werkelijkheid ophoudt te bestaan.")]
         public LocalDateTime? EndDateTime { get; }
+        
+        [EventPropertyDescription("Tijdstip waarop het object werd ingevoerd in de databank.")] 
         public Instant Timestamp { get; }
+        
+        [EventPropertyDescription("Operator door wie het object werd ingevoerd in de databank.")]
         public string Operator { get; }
+        
+        [EventPropertyDescription("Bewerking waarmee het object werd ingevoerd in de databank.")] 
         public CrabModification? Modification { get; }
+        
+        [EventPropertyDescription("Organisatie die het object heeft ingevoerd in de databank.")]
         public CrabOrganisation? Organisation { get; }
 
         public AddressSubaddressWasImportedFromCrab(

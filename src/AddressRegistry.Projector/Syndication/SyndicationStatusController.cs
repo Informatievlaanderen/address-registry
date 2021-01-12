20b445f20efdcba984f8292db73bc6fddea5848b
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2021-01-12 10:46:24 +0100

    feat: add syndication status to projector api GRAR-1567

---
+++ src/AddressRegistry.Projector/Syndication/SyndicationStatusController.cs
@@ -0,0 +1,50 @@
+namespace AddressRegistry.Projector.Syndication
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Api;
+    using Be.Vlaanderen.Basisregisters.ProjectionHandling.Runner.ProjectionStates;
+    using Dapper;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Data.SqlClient;
+    using Microsoft.Extensions.Configuration;
+
+    [ApiVersion("1.0")]
+    [ApiRoute("syndication")]
+    public class SyndicationStatusController : ApiController
+    {
+        private const string? SyndicationConnectionStringKey = "Syndication";
+
+        [HttpGet]
+        public async Task<IActionResult> Get(
+            [FromServices] IConfiguration configuration,
+            CancellationToken cancellationToken)
+        {
+            List<SyndicationStatusResponse> response;
+            await using (var sqlConnection = new SqlConnection(configuration.GetConnectionString(SyndicationConnectionStringKey)))
+            {
+                var result =
+                    await sqlConnection.QueryAsync<ProjectionStateItem>(
+                        "SELECT * FROM [AddressRegistrySyndication].[ProjectionStates]", cancellationToken);
+
+                response = result.Select(x => new SyndicationStatusResponse(x)).ToList();
+            }
+
+            return Ok(response);
+        }
+    }
+
+    public class SyndicationStatusResponse
+    {
+        public string ProjectionName { get; set; }
+        public long Position { get; set; }
+
+        public SyndicationStatusResponse(ProjectionStateItem projectionStateItem)
+        {
+            ProjectionName = projectionStateItem.Name;
+            Position = projectionStateItem.Position;
+        }
+    }
+}

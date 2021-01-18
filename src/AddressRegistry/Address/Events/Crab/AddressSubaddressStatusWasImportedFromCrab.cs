59ad2c2c04a108790824d59c6647ae01be4d8173
AnaisDC <49196256+AnaisDC@users.noreply.github.com>
2021-01-18 10:24:19 +0100

    Update AddressSubaddressStatusWasImportedFromCrab.cs

--- src/AddressRegistry/Address/Events/Crab/AddressSubaddressStatusWasImportedFromCrab.cs
+++ src/AddressRegistry/Address/Events/Crab/AddressSubaddressStatusWasImportedFromCrab.cs
@@ -36,7 +36,8 @@ namespace AddressRegistry.Address.Events.Crab
         
         [EventPropertyDescription("Organisatie die het object heeft ingevoerd in de databank.")]
         public CrabOrganisation? Organisation { get; }
-
+        
+        [EventPropertyDescription("Unieke sleutel.")]
         public int Key => SubaddressStatusId;
 
         public AddressSubaddressStatusWasImportedFromCrab(

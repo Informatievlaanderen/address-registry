a532a0c7d346b8696f159aa435cd5b2c9dbfb360
Arne Dumarey <dumarey.arne@gmail.com>
2021-04-28 10:37:06 +0200

    feat: add status filter to legacy list

---
+++ src/AddressRegistry.Projections.Legacy/Migrations/20210428083448_AddIndexStatusOnList.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AddressRegistry.Projections.Legacy.Migrations
+{
+    public partial class AddIndexStatusOnList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AddressList_Status",
+                schema: "AddressRegistryLegacy",
+                table: "AddressList",
+                column: "Status");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AddressList_Status",
+                schema: "AddressRegistryLegacy",
+                table: "AddressList");
+        }
+    }
+}

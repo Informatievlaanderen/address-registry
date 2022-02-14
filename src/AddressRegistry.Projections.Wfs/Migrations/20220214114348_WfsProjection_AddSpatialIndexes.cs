a5951b3f61137a1aec914058e5691a6dacf1adf3
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2022-02-14 12:45:06 +0100

    fix: add spacial indexes

---
+++ src/AddressRegistry.Projections.Wfs/Migrations/20220214114348_WfsProjection_AddSpatialIndexes.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AddressRegistry.Projections.Wfs.Migrations
+{
+    public partial class WfsProjection_AddSpatialIndexes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+	            CREATE SPATIAL INDEX [SPATIAL_Address_Position] ON [wfs.address].[AddressDetails] ([Position])
+	            USING  GEOMETRY_GRID
+	            WITH (
+		            BOUNDING_BOX =(22279.17, 153050.23, 258873.3, 244022.31),
+		            GRIDS =(
+			            LEVEL_1 = MEDIUM,
+			            LEVEL_2 = MEDIUM,
+			            LEVEL_3 = MEDIUM,
+			            LEVEL_4 = MEDIUM),
+	            CELLS_PER_OBJECT = 5)
+	            GO");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex("[SPATIAL_Address_Position]");
+        }
+    }
+}

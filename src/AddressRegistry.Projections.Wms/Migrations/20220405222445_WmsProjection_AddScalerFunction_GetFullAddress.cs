ac337c0549d48aba23cd22226ac931dfdfe6458f
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2022-04-06 00:42:19 +0200

    fix: bugfix paused wms projection, add adres views, scaler & table-valued functions

---
+++ src/AddressRegistry.Projections.Wms/Migrations/20220405222445_WmsProjection_AddScalerFunction_GetFullAddress.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AddressRegistry.Projections.Wms.Migrations
+{
+    public partial class WmsProjection_AddScalerFunction_GetFullAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE FUNCTION [wms.address].[GetFullAddress](
+    @StreetName NVARCHAR(max),
+    @HouseNumber NVARCHAR(max),
+    @BoxNumber NVARCHAR(max),
+    @PostalCode NVARCHAR(max),
+    @Municipality NVARCHAR(max)
+)
+RETURNS NVARCHAR(max) WITH SCHEMABINDING
+AS
+BEGIN
+    IF @StreetName IS NOT NULL AND @BoxNumber IS NULL
+        RETURN CONCAT(@StreetName, ' ',  @HouseNumber, ', ', @PostalCode, ' ', @Municipality);
+
+    IF @StreetName IS NOT NULL AND @BoxNumber IS NOT NULL
+        RETURN CONCAT(@StreetName, ' ',  @HouseNumber, ' bus ', @BoxNumber, ', ', @PostalCode, ' ', @Municipality);
+
+    RETURN NULL;
+END");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+IF OBJECT_ID (N'[wms.address].[GetFullAddress]', N'FN') IS NOT NULL
+    DROP FUNCTION [wms.address].[GetFullAddress];");
+        }
+    }
+}
---
+++ src/AddressRegistry.Projections.Wms/Migrations/20220405223006_WmsProjection_AddScalerFunction_GetHouseNumberLabel.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AddressRegistry.Projections.Wms.Migrations
+{
+    public partial class WmsProjection_AddScalerFunction_GetHouseNumberLabel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE FUNCTION [wms.address].[GetHouseNumberLabel](@Position geometry, @Status NVARCHAR(max), @Removed bit)
+RETURNS NVARCHAR(max) WITH SCHEMABINDING
+AS
+BEGIN
+    DECLARE @HouseNumberLabel NVARCHAR(max)
+    SET @HouseNumberLabel = NULL
+
+    IF @Removed = 1 OR @Position IS NULL
+        RETURN @HouseNumberLabel;
+
+    SELECT
+        @HouseNumberLabel = CASE
+                    WHEN COUNT(HouseNumber.Label) = 0
+                        THEN NULL
+                    WHEN Min(HouseNumber.Label) = Max(HouseNumber.Label)
+                        THEN Min(HouseNumber.Label)
+                        ELSE CONCAT(Min(HouseNumber.Label), '-' , Max(HouseNumber.Label))
+                END
+    FROM
+        (
+            SELECT
+            CASE
+                WHEN BoxNumber IS NULL
+                THEN HouseNumber
+                ELSE CONCAT(HouseNumber, '/', BoxNumber)
+            END as Label
+            FROM [wms.address].[AddressDetails]
+            WHERE [Position].STDistance(@Position) < 1
+                AND [Status] = @Status
+                AND [Removed] = 0
+                AND [Complete] = 1
+        ) as HouseNumber
+    RETURN @HouseNumberLabel;
+END");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+IF OBJECT_ID (N'[wms.address].[GetHouseNumberLabel]', N'FN') IS NOT NULL
+    DROP FUNCTION [wms.address].[GetHouseNumberLabel];  ");
+        }
+    }
+}

cc23b1c82e0406d9e20cb20a9438bef6c80806f3
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2022-04-06 23:43:56 +0200

    fix: migration fix

--- src/AddressRegistry.Projections.Wms/Migrations/20220405223006_WmsProjection_AddScalerFunction_GetHouseNumberLabel.cs
+++ src/AddressRegistry.Projections.Wms/Migrations/20220405223006_WmsProjection_AddScalerFunction_GetHouseNumberLabel.cs
@@ -13,28 +13,33 @@ CREATE FUNCTION [wms.address].[GetHouseNumberLabel](@Position geometry, @Status 
 RETURNS NVARCHAR(max) WITH SCHEMABINDING
 AS
 BEGIN
-    DECLARE @HouseNumberLabel NVARCHAR(max)
+    DECLARE
+        @HouseNumberLabel NVARCHAR(max),
+        @HouseNumberCount INT
+
     SET @HouseNumberLabel = NULL
+    SET @HouseNumberCount = 0
 
     IF @Removed = 1 OR @Position IS NULL
         RETURN @HouseNumberLabel;
 
+    SET @HouseNumberCount = [wms.address].[CountHouseNumberByPosition](@Position, @Status)
+
     SELECT
-        @HouseNumberLabel = CASE
-                    WHEN COUNT(HouseNumber.Label) = 0
-                        THEN NULL
-                    WHEN Min(HouseNumber.Label) = Max(HouseNumber.Label)
-                        THEN Min(HouseNumber.Label)
-                        ELSE CONCAT(Min(HouseNumber.Label), '-' , Max(HouseNumber.Label))
-                END
+        @HouseNumberLabel =
+        CASE
+                WHEN @HouseNumberCount = 0
+                    THEN NULL
+                WHEN @HouseNumberCount = 1
+                    THEN Min(HouseNumber.Label)
+                WHEN Min(HouseNumber.Label) = Max(HouseNumber.Label)
+                    THEN Min(HouseNumber.Label)
+                    ELSE CONCAT(Min(HouseNumber.Label), '-' , Max(HouseNumber.Label))
+            END
     FROM
         (
             SELECT
-            CASE
-                WHEN BoxNumber IS NULL
-                THEN HouseNumber
-                ELSE CONCAT(HouseNumber, '/', BoxNumber)
-            END as Label
+                HouseNumber as Label
             FROM [wms.address].[AddressDetails]
             WHERE [Position].STDistance(@Position) < 1
                 AND [Status] = @Status

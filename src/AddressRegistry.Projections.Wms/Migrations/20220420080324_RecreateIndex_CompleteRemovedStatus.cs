a78e9ad357454ad97ae9a53ef86e89bef80d56e3
Arne Dumarey <dumarey.arne@gmail.com>
2022-04-20 10:07:20 +0200

    fix: recreate index wms GAWR-3003

---
+++ src/AddressRegistry.Projections.Wms/Migrations/20220420080324_RecreateIndex_CompleteRemovedStatus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AddressRegistry.Projections.Wms.Migrations
+{
+    public partial class RecreateIndex_CompleteRemovedStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AddressDetails_Removed_Complete",
+                schema: "wms.address",
+                table: "AddressDetails");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AddressDetails_Removed_Complete_Status",
+                schema: "wms.address",
+                table: "AddressDetails",
+                columns: new[] { "Removed", "Complete", "Status" })
+                .Annotation("SqlServer:Include", new[] { "StreetNameId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AddressDetails_Removed_Complete_Status",
+                schema: "wms.address",
+                table: "AddressDetails");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AddressDetails_Removed_Complete",
+                schema: "wms.address",
+                table: "AddressDetails",
+                columns: new[] { "Removed", "Complete" });
+        }
+    }
+}

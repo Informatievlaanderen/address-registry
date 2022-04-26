1aba72528dae1306e51f8eef7aecbd739d3907f8
Arne Dumarey <dumarey.arne@gmail.com>
2022-04-26 10:44:52 +0200

    chore: clean up unused backoffice params

--- src/AddressRegistry.Api.Backoffice/Infrastructure/Options/ResponseOptions.cs
+++ src/AddressRegistry.Api.Backoffice/Infrastructure/Options/ResponseOptions.cs
@@ -2,7 +2,6 @@ namespace AddressRegistry.Api.BackOffice.Infrastructure.Options
 {
     public class ResponseOptions
     {
-        public string Naamruimte { get; set; }
         public string DetailUrl { get; set; }
     }
 }

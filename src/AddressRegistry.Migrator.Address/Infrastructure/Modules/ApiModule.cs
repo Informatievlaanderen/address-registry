cd66dec000b496a8cfae85e8a18ba176677b6fab
Arne Dumarey <dumarey.arne@gmail.com>
2022-04-29 15:50:25 +0200

    fix: backoffice scope lifetime in migrator

--- src/AddressRegistry.Migrator.Address/Infrastructure/Modules/ApiModule.cs
+++ src/AddressRegistry.Migrator.Address/Infrastructure/Modules/ApiModule.cs
@@ -3,6 +3,7 @@ namespace AddressRegistry.Migrator.Address.Infrastructure.Modules
     using AddressRegistry.Address;
     using AddressRegistry.Infrastructure;
     using AddressRegistry.Infrastructure.Modules;
+    using Api.BackOffice;
     using Api.BackOffice.Infrastructure.Modules;
     using Autofac;
     using Autofac.Extensions.DependencyInjection;
@@ -11,6 +12,7 @@ namespace AddressRegistry.Migrator.Address.Infrastructure.Modules
     using Be.Vlaanderen.Basisregisters.EventHandling.Autofac;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.SqlStreamStore.Autofac;
     using Consumer;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
@@ -33,13 +35,21 @@ namespace AddressRegistry.Migrator.Address.Infrastructure.Modules
 
         protected override void Load(ContainerBuilder builder)
         {
+            var projectionsConnectionString = _configuration.GetConnectionString("BackOffice");
+            _services
+                .AddDbContext<BackOfficeContext>(options => options
+                        .UseLoggerFactory(_loggerFactory)
+                        .UseSqlServer(projectionsConnectionString, sqlServerOptions => sqlServerOptions
+                            .EnableRetryOnFailure()
+                            .MigrationsHistoryTable(MigrationTables.BackOffice, Schema.BackOffice))
+                    , ServiceLifetime.Transient);
+
             var eventSerializerSettings = EventsJsonSerializerSettingsProvider.CreateSerializerSettings();
 
             builder
                 .RegisterModule(new DataDogModule(_configuration))
                 .RegisterModule<EnvelopeModule>()
                 .RegisterModule(new EventHandlingModule(typeof(DomainAssemblyMarker).Assembly, eventSerializerSettings))
-                .RegisterModule(new BackOfficeModule(_configuration, _services, _loggerFactory))
                 .RegisterModule(new ConsumerModule(_configuration, _services, _loggerFactory))
                 .RegisterModule(new EditModule(_configuration, _services, _loggerFactory));
 

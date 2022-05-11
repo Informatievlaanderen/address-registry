cb324c99d66608bd2782627dbbec85a829cb99e2
Arne Dumarey <dumarey.arne@gmail.com>
2022-05-11 15:49:44 +0200

    fix: copy consumer and add logging

--- src/AddressRegistry.Consumer/Consumer.cs
+++ src/AddressRegistry.Consumer/Consumer.cs
@@ -1,13 +1,18 @@
 namespace AddressRegistry.Consumer
 {
+    using System;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using Autofac;
     using Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Simple;
+    using Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Simple.Extensions;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.Connector;
     using Confluent.Kafka;
     using Microsoft.Extensions.Logging;
+    using Newtonsoft.Json;
     using Projections;
+    using Serilog;
 
     public class Consumer
     {
@@ -37,7 +42,7 @@ namespace AddressRegistry.Consumer
             var projector = new ConnectedProjector<CommandHandler>(Resolve.WhenEqualToHandlerMessageType(new StreetNameKafkaProjection().Handlers));
 
             var consumerGroupId = $"{nameof(AddressRegistry)}.{nameof(Consumer)}.{_consumerOptions.Topic}{_consumerOptions.ConsumerGroupSuffix}";
-            return KafkaConsumer.Consume(
+            return KafkaConsumerV2.Consume(
                 _options,
                 consumerGroupId,
                 _consumerOptions.Topic,
@@ -54,6 +59,83 @@ namespace AddressRegistry.Consumer
             //    var logger = _loggerFactory.CreateLogger<Consumer>();
             //    logger.LogCritical($"Consumer group {consumerGroupId} could not consume from topic {_consumerOptions.Topic}");
             //}
+        }
+    }
+
+    public static class KafkaConsumerV2
+    {
+        public static async Task<Result<KafkaJsonMessage>> Consume(
+            KafkaOptions options,
+            string consumerGroupId,
+            string topic,
+            Func<object, Task> messageHandler,
+            Offset? offset = null,
+            CancellationToken cancellationToken = default)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = options.BootstrapServers,
+                GroupId = consumerGroupId,
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnableAutoCommit = false
+            }.WithAuthentication(options);
+
+            var serializer = JsonSerializer.CreateDefault(options.JsonSerializerSettings);
+
+            var consumerBuilder = new ConsumerBuilder<Ignore, string>(config)
+                .SetValueDeserializer(Deserializers.Utf8);
+            if (offset.HasValue)
+            {
+                consumerBuilder.SetPartitionsAssignedHandler((cons, topicPartitions) =>
+                {
+                    var partitionOffset = topicPartitions.Select(x => new TopicPartitionOffset(x.Topic, x.Partition, offset.Value));
+                    return partitionOffset;
+                });
+            }
+
+            var kafkaJsonMessage = new KafkaJsonMessage("", "");
+            using var consumer = consumerBuilder.Build();
+            try
+            {
+                Log.Information("Subscribing to topic");
+                consumer.Subscribe(topic);
+                Log.Information("Subscribed to topic");
+
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    Log.Information("Consuming topic");
+                    var consumeResult = consumer.Consume(TimeSpan.FromSeconds(3));
+                    if (consumeResult == null) //if no message is found, returns null
+                    {
+                        await Task.Delay(1000, cancellationToken);
+                        continue;
+                    }
+
+                    Log.Information("Consuming message");
+                    kafkaJsonMessage = serializer.Deserialize<KafkaJsonMessage>(consumeResult.Message.Value) ?? throw new ArgumentException("Kafka json message is null.");
+                    var messageData = kafkaJsonMessage.Map() ?? throw new ArgumentException("Kafka message data is null.");
+
+                    await messageHandler(messageData);
+                    Log.Information("Committing message");
+                    consumer.Commit(consumeResult);
+                }
+
+                return Result<KafkaJsonMessage>.Success(kafkaJsonMessage);
+            }
+            catch (ConsumeException ex)
+            {
+                Log.Error(ex, "ConsumeException");
+                return Result<KafkaJsonMessage>.Failure(ex.Error.Code.ToString(), ex.Error.Reason);
+            }
+            catch (OperationCanceledException ex)
+            {
+                Log.Error(ex, "OperationCanceledException");
+                return Result<KafkaJsonMessage>.Success(kafkaJsonMessage);
+            }
+            finally
+            {
+                consumer.Unsubscribe();
+            }
         }
     }
 }

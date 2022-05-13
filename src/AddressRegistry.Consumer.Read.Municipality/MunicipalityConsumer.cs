146435c70f18d7f7ff6bc3d891d955558f07b219
Arne Dumarey <dumarey.arne@gmail.com>
2022-05-13 11:22:06 +0200

    fix: upgrade kafka consumer

--- src/AddressRegistry.Consumer.Read.Municipality/MunicipalityConsumer.cs
+++ src/AddressRegistry.Consumer.Read.Municipality/MunicipalityConsumer.cs
@@ -28,29 +28,33 @@ namespace AddressRegistry.Consumer.Read.Municipality
             _municipalityConsumerOptions = municipalityConsumerOptions;
         }
 
-        public async Task Start(CancellationToken cancellationToken = default)
+        public Task Start(CancellationToken cancellationToken = default)
         {
             //var projector = new ConnectedProjector<Func<ConsumerContext>>(Resolve.WhenEqualToHandlerMessageType(new MunicipalityProjections().Handlers));
             var projector = new ConnectedProjector<MunicipalityConsumerContext>(Resolve.WhenEqualToHandlerMessageType(new MunicipalityProjections().Handlers));
 
             var consumerGroupId = $"{nameof(AddressRegistry)}.{nameof(MunicipalityConsumer)}.{_municipalityConsumerOptions.Topic}{_municipalityConsumerOptions.ConsumerGroupSuffix}";
-            var result = await KafkaConsumer.Consume(
-                _options,
-                consumerGroupId,
-                _municipalityConsumerOptions.Topic,
-                async message =>
-                {
-                    //await projector.ProjectAsync(_container.Resolve<Func<ConsumerContext>>(), message, cancellationToken);
-                    await projector.ProjectAsync(_container.Resolve<MunicipalityConsumerContext>(), message, cancellationToken);
-                },
-                offset: null,
+            return KafkaConsumer.Consume(
+                new KafkaConsumerOptions(
+                    _options.BootstrapServers,
+                    _options.SaslUserName,
+                    _options.SaslPassword,
+                    consumerGroupId,
+                    _municipalityConsumerOptions.Topic,
+                    async message =>
+                    {
+                        await projector.ProjectAsync(_container.Resolve<MunicipalityConsumerContext>(), message, cancellationToken);
+                    },
+                    noMessageFoundDelay: 300,
+                    offset: null,
+                    _options.JsonSerializerSettings),
                 cancellationToken);
 
-            if (!result.IsSuccess)
-            {
-                var logger = _loggerFactory.CreateLogger<MunicipalityConsumer>();
-                logger.LogCritical($"Consumer group {consumerGroupId} could not consume from topic {_municipalityConsumerOptions.Topic}");
-            }
+            //if (!result.IsSuccess)
+            //{
+            //    var logger = _loggerFactory.CreateLogger<MunicipalityConsumer>();
+            //    logger.LogCritical($"Consumer group {consumerGroupId} could not consume from topic {_municipalityConsumerOptions.Topic}");
+            //}
         }
     }
 }

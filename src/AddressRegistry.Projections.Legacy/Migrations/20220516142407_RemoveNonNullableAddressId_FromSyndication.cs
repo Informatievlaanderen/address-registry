8761783556b29ced1bf47965337e647947c3b869
pgallik <patrickgallik@gmail.com>
2022-05-16 16:20:47 +0200

    fix: syndication projection set AddressId nullable in db

---
+++ src/AddressRegistry.Projections.Legacy/Migrations/20220516142407_RemoveNonNullableAddressId_FromSyndication.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AddressRegistry.Projections.Legacy.Migrations
+{
+    public partial class RemoveNonNullableAddressId_FromSyndication : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AddressId",
+                schema: "AddressRegistryLegacy",
+                table: "AddressSyndication",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AddressId",
+                schema: "AddressRegistryLegacy",
+                table: "AddressSyndication",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}

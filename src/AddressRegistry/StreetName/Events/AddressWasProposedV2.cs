e4043f0a9cb5a02ae75827fc3a59db406f1f0dce
Arne Dumarey <dumarey.arne@gmail.com>
2022-05-17 15:47:50 +0200

    fix: set event descriptions for AddressWasProposedV2

--- src/AddressRegistry/StreetName/Events/AddressWasProposedV2.cs
+++ src/AddressRegistry/StreetName/Events/AddressWasProposedV2.cs
@@ -8,18 +8,30 @@ namespace AddressRegistry.StreetName.Events
 
     [EventTags(EventTag.For.Sync, EventTag.For.Edit)]
     [EventName(EventName)]
-    [EventDescription("De adres werd voorgesteld.")]
+    [EventDescription("Het adres werd voorgesteld.")]
     public class AddressWasProposedV2 : IStreetNameEvent
     {
         public const string EventName = "AddressWasProposedV2"; // BE CAREFUL CHANGING THIS!!
 
+        [EventPropertyDescription("Objectidentificator van de straatnaam aan dewelke het adres is toegewezen.")]
         public int StreetNamePersistentLocalId { get; }
+
+        [EventPropertyDescription("Objectidentificator van het adres.")]
         public int AddressPersistentLocalId { get; }
+
+        [EventPropertyDescription("Objectidentificator van het huisnummer adres waaraan het busnummer gelinkt is.")]
         public int? ParentPersistentLocalId { get; }
+
+        [EventPropertyDescription("Postcode (= objectidentificator) van het PostInfo-object dat deel uitmaakt van het adres.")]
         public string PostalCode { get; }
+
+        [EventPropertyDescription("Huisnummer van het adres.")]
         public string HouseNumber { get; }
+
+        [EventPropertyDescription("Busnummer van het adres.")]
         public string? BoxNumber { get; }
 
+        [EventPropertyDescription("Metadata bij het event.")]
         public ProvenanceData Provenance { get; private set; }
 
         public AddressWasProposedV2(

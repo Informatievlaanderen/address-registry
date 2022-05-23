a5fc77d72acdf71e91e116bff7902d432de73703
Arne Dumarey <dumarey.arne@gmail.com>
2022-05-23 16:31:38 +0200

    fix: correct clone streetnamepersistentlocalid in legacy sync

--- src/AddressRegistry.Projections.Legacy/AddressSyndication/AddressSyndication.cs
+++ src/AddressRegistry.Projections.Legacy/AddressSyndication/AddressSyndication.cs
@@ -69,6 +69,7 @@ namespace AddressRegistry.Projections.Legacy.AddressSyndication
 
                 AddressId = AddressId,
                 PersistentLocalId = PersistentLocalId,
+                StreetNamePersistentLocalId = StreetNamePersistentLocalId,
                 StreetNameId = StreetNameId,
                 PostalCode = PostalCode,
                 HouseNumber = HouseNumber,

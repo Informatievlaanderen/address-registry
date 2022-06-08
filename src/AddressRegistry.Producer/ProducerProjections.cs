080de14afb14506105c3ec4117f2bed5c83fdcc7
Arne Dumarey <dumarey.arne@gmail.com>
2022-06-08 09:37:59 +0200

    fix: don't reproduce streetname events

--- src/AddressRegistry.Producer/ProducerProjections.cs
+++ src/AddressRegistry.Producer/ProducerProjections.cs
@@ -207,26 +207,6 @@ namespace AddressRegistry.Producer
             {
                 await Produce(message.Message.StreetNamePersistentLocalId, message.Message.ToContract(), ct);
             });
-
-            When<Be.Vlaanderen.Basisregisters.ProjectionHandling.SqlStreamStore.Envelope<StreetName.MigratedStreetNameWasImported>>(async (_, message, ct) =>
-            {
-                await Produce(message.Message.StreetNamePersistentLocalId, message.Message.ToContract(), ct);
-            });
-
-            When<Be.Vlaanderen.Basisregisters.ProjectionHandling.SqlStreamStore.Envelope<StreetName.StreetNameWasApproved>>(async (_, message, ct) =>
-            {
-                await Produce(message.Message.StreetNamePersistentLocalId, message.Message.ToContract(), ct);
-            });
-
-            When<Be.Vlaanderen.Basisregisters.ProjectionHandling.SqlStreamStore.Envelope<StreetName.StreetNameWasImported>>(async (_, message, ct) =>
-            {
-                await Produce(message.Message.StreetNamePersistentLocalId, message.Message.ToContract(), ct);
-            });
-
-            When<Be.Vlaanderen.Basisregisters.ProjectionHandling.SqlStreamStore.Envelope<StreetName.StreetNameWasRemoved>>(async (_, message, ct) =>
-            {
-                await Produce(message.Message.StreetNamePersistentLocalId, message.Message.ToContract(), ct);
-            });
         }
 
         private async Task Produce<T>(Guid guid, T message, CancellationToken cancellationToken = default)

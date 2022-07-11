57464b7dd0cdac92d4b21aad52357255de97adce
pgallik <patrickgallik@gmail.com>
2022-07-11 16:08:51 +0200

    assembly info

---
+++ src/AddressRegistry.Api.BackOffice.Abstractions/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("AddressRegistry BackOffice Abstractions")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("1d54b910-9f78-41d7-8bc5-2c0f7d349716")]

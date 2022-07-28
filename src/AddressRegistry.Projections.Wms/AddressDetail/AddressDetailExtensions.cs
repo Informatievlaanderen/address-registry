def255b7a782fe9c5de6e6d8cb797b7f84b8abc7
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-07-28 14:25:40 +0200

    fix: remove unused members

--- src/AddressRegistry.Projections.Wms/AddressDetail/AddressDetailExtensions.cs
+++ src/AddressRegistry.Projections.Wms/AddressDetail/AddressDetailExtensions.cs
@@ -2,20 +2,11 @@ namespace AddressRegistry.Projections.Wms.AddressDetail
 {
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.Connector;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.EntityFrameworkCore.Metadata.Internal;
-    using NetTopologySuite.Geometries;
-    using NetTopologySuite.IO;
 
     public static class AddressDetailExtensions
     {
-        private static string? STAsText(this Point point)
-            => point != null ? WKTWriter.ToPoint(point.Coordinate) : null;
-
         private static async Task<AddressDetailItem> FindAddressDetail(
             this WmsContext context,
             Guid addressId,
@@ -26,11 +17,15 @@ namespace AddressRegistry.Projections.Wms.AddressDetail
             var address = await context.AddressDetail.FindAsync(addressId, cancellationToken: ct);
 
             if (address == null)
+            {
                 throw DatabaseItemNotFound(addressId);
+            }
 
             // exclude soft deleted entries, unless allowed
-            if(!address.Removed || allowRemovedAddress)
+            if (!address.Removed || allowRemovedAddress)
+            {
                 return address;
+            }
 
             throw DatabaseItemNotFound(addressId);
         }
@@ -47,28 +42,8 @@ namespace AddressRegistry.Projections.Wms.AddressDetail
             return address;
         }
 
-        private static string? GetHouseNumberLabel(this IList<AddressDetailItem>? addresses)
-        {
-            if (addresses == null)
-                return null;
-
-            var orderedAddresses = addresses
-                .Where(i => !string.IsNullOrWhiteSpace(i.HouseNumber) && i.Removed == false)
-                .OrderBy(i => i.HouseNumber)
-                .ToList();
-
-            var smallestNumber = orderedAddresses.FirstOrDefault()?.HouseNumber;
-            var highestNumber = orderedAddresses.LastOrDefault()?.HouseNumber;
-
-            var houseNumber = smallestNumber;
-            if (string.IsNullOrEmpty(highestNumber) && smallestNumber != highestNumber)
-                houseNumber = $"{smallestNumber}-{highestNumber}";
-
-            return houseNumber;
-        }
-
-        private static ProjectionItemNotFoundException<AddressDetailProjections> DatabaseItemNotFound(Guid addressId)
-            => new(addressId.ToString("D"));
+        private static ProjectionItemNotFoundException<AddressDetailProjections> DatabaseItemNotFound(Guid addressId) =>
+            new ProjectionItemNotFoundException<AddressDetailProjections>(addressId.ToString("D"));
 
     }
 }

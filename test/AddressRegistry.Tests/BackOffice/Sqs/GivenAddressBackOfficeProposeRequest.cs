6d7f064acd05764414d19caced26034f40d38947
jvandaal <jonas.vandaal@outlook.be>
2022-09-23 09:11:02 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:test/AddressRegistry.Tests/BackOffice/Sqs/GivenAddressBackOfficeProposeRequest.cs

--- test/AddressRegistry.Tests/BackOffice/Sqs/GivenAddressBackOfficeProposeRequest.cs
+++ test/AddressRegistry.Tests/BackOffice/Sqs/GivenAddressBackOfficeProposeRequest.cs
@@ -1,8 +1,6 @@
 namespace AddressRegistry.Tests.BackOffice.Sqs
 {
-    using AddressRegistry.Api.BackOffice.Abstractions.Exceptions;
     using AddressRegistry.Api.BackOffice.Handlers.Sqs;
-    using Infrastructure;
     using Be.Vlaanderen.Basisregisters.MessageHandling.AwsSqs.Simple;
     using FluentAssertions;
     using global::AutoFixture;
@@ -15,19 +13,14 @@ namespace AddressRegistry.Tests.BackOffice.Sqs
     using AddressRegistry.Api.BackOffice.Handlers.Sqs.Handlers;
     using AddressRegistry.Api.BackOffice.Handlers.Sqs.Requests;
     using AutoFixture;
-    using Consumer.Read.Municipality.Projections;
-    using StreetName;
     using TicketingService.Abstractions;
     using Xunit;
     using Xunit.Abstractions;
 
     public sealed class GivenAddressBackOfficeProposeRequest : AddressRegistryTest
     {
-        private readonly TestMunicipalityConsumerContext _municipalityConsumerContext;
-
         public GivenAddressBackOfficeProposeRequest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
         {
-            _municipalityConsumerContext = new FakeMunicipalityConsumerContextFactory().CreateDbContext();
             Fixture.Customize(new WithFixedMunicipalityId());
         }
 
@@ -35,18 +28,10 @@ namespace AddressRegistry.Tests.BackOffice.Sqs
         public async Task ThenTicketWithLocationIsCreated()
         {
             // Arrange
-            var nisCode = new NisCode("1234");
-
-            _municipalityConsumerContext.MunicipalityLatestItems.Add(new MunicipalityLatestItem
-            {
-                MunicipalityId = Fixture.Create<MunicipalityId>(),
-                NisCode = nisCode
-            });
-
-            await _municipalityConsumerContext.SaveChangesAsync();
+            var streetNameId = 45041;
 
             var request = Fixture.Create<AddressBackOfficeProposeRequest>();
-            request.PostInfoId = $"https://data.vlaanderen.be/id/gemeente/{nisCode}";
+            request.StraatNaamId = $"https://data.vlaanderen.be/id/straatnaam/{streetNameId}";
             var sqsRequest = new SqsAddressProposeRequest { Request = request };
 
             var ticketId = Fixture.Create<Guid>();
@@ -62,8 +47,7 @@ namespace AddressRegistry.Tests.BackOffice.Sqs
             var sut = new SqsAddressProposeHandler(
                 sqsQueue.Object,
                 ticketingMock.Object,
-                ticketingUrl,
-                _municipalityConsumerContext);
+                ticketingUrl);
 
             // Act
             var result = await sut.Handle(sqsRequest, CancellationToken.None);
@@ -72,30 +56,9 @@ namespace AddressRegistry.Tests.BackOffice.Sqs
             sqsRequest.TicketId.Should().Be(ticketId);
             sqsQueue.Verify(x => x.Copy(
                 sqsRequest,
-                It.Is<SqsQueueOptions>(y => y.MessageGroupId == ((Guid) Fixture.Create<MunicipalityId>()).ToString("D")),
+                It.Is<SqsQueueOptions>(y => y.MessageGroupId == streetNameId.ToString()),
                 CancellationToken.None));
             result.Location.Should().Be(ticketingUrl.For(ticketId));
-        }
-
-        [Fact]
-        public void ForNotExistingMunicipality_ThrowsAggregateIdNotFound()
-        {
-            // Arrange
-            var sut = new SqsAddressProposeHandler(
-                Mock.Of<ISqsQueue>(),
-                Mock.Of<ITicketing>(),
-                Mock.Of<ITicketingUrl>(),
-                _municipalityConsumerContext);
-
-            // Act
-            var act = async () => await sut.Handle(
-                new SqsAddressProposeRequest { Request = Fixture.Create<AddressBackOfficeProposeRequest>() },
-                CancellationToken.None);
-
-            // Assert
-            act
-                .Should()
-                .ThrowAsync<AggregateIdIsNotFoundException>();
         }
     }
 }

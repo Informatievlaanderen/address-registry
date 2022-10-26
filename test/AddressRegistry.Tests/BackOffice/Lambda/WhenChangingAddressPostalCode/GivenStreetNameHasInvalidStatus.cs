bb607a0cd2d63982cf2235b9a0cd2b46a6a2fce5
Arne Dumarey <dumarey.arne@gmail.com>
2022-10-26 10:39:25 +0200

    fix: add streetname status validation for address changes
    GAWR-3893 GAWR-3894

---
+++ test/AddressRegistry.Tests/BackOffice/Lambda/WhenChangingAddressPostalCode/GivenStreetNameHasInvalidStatus.cs
@@ -0,0 +1,105 @@
+namespace AddressRegistry.Tests.BackOffice.Lambda.WhenChangingAddressPostalCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AddressRegistry.Api.BackOffice.Abstractions.Requests;
+    using AddressRegistry.Api.BackOffice.Handlers.Lambda.Handlers;
+    using AddressRegistry.Api.BackOffice.Handlers.Lambda.Requests;
+    using AddressRegistry.Api.BackOffice.Handlers.Sqs.Requests;
+    using Autofac;
+    using AutoFixture;
+    using BackOffice.Infrastructure;
+    using Be.Vlaanderen.Basisregisters.GrAr.Provenance;
+    using Consumer.Read.Municipality.Projections;
+    using global::AutoFixture;
+    using Infrastructure;
+    using Microsoft.Extensions.Configuration;
+    using Moq;
+    using Projections.Syndication.PostalInfo;
+    using StreetName;
+    using StreetName.Exceptions;
+    using TicketingService.Abstractions;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class GivenStreetNameHasInvalidStatus : BackOfficeLambdaTest
+    {
+        private readonly TestSyndicationContext _syndicationContext;
+        private readonly TestMunicipalityConsumerContext _municipalityContext;
+
+        public GivenStreetNameHasInvalidStatus(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            Fixture.Customize(new WithFixedMunicipalityId());
+
+            _syndicationContext = new FakeSyndicationContextFactory().CreateDbContext();
+            _municipalityContext = new FakeMunicipalityConsumerContextFactory().CreateDbContext();
+        }
+
+        [Fact]
+        public async Task WhenStreetNameIsRemoved_ThenTicketingErrorIsExpected()
+        {
+            // Arrange
+            var streetNamePersistentLocalId = Fixture.Create<StreetNamePersistentLocalId>();
+            var addressPersistentLocalId = Fixture.Create<AddressPersistentLocalId>();
+
+            var postInfoId = "2018";
+            var correctPostInfoId = "2019";
+            var nisCode = Fixture.Create<NisCode>();
+            var municipalityId = Fixture.Create<MunicipalityId>();
+
+            _syndicationContext.PostalInfoLatestItems.Add(new PostalInfoLatestItem
+            {
+                PostalCode = postInfoId,
+                NisCode = nisCode
+            });
+            _syndicationContext.PostalInfoLatestItems.Add(new PostalInfoLatestItem
+            {
+                PostalCode = correctPostInfoId,
+                NisCode = nisCode
+            });
+            await _syndicationContext.SaveChangesAsync();
+
+            _municipalityContext.MunicipalityLatestItems.Add(new MunicipalityLatestItem
+            {
+                MunicipalityId = municipalityId,
+                NisCode = nisCode
+            });
+            await _municipalityContext.SaveChangesAsync();
+
+            var ticketing = new Mock<ITicketing>();
+
+            var sut = new ChangeAddressPostalCodeLambdaHandler(
+                Container.Resolve<IConfiguration>(),
+                new FakeRetryPolicy(),
+                ticketing.Object,
+                Mock.Of<IStreetNames>(),
+                MockExceptionIdempotentCommandHandler<StreetNameHasInvalidStatusException>().Object);
+
+            // Act
+            var request = new ChangeAddressPostalCodeLambdaRequest(Fixture.Create<int>().ToString(),
+                new ChangeAddressPostalCodeSqsRequest
+                {
+                    Request = new ChangeAddressPostalCodeBackOfficeRequest
+                    {
+                        PostInfoId = PostInfoPuri + correctPostInfoId
+                    },
+                    PersistentLocalId = addressPersistentLocalId,
+                    TicketId = Guid.NewGuid(),
+                    Metadata = new Dictionary<string, object?>(),
+                    ProvenanceData = Fixture.Create<ProvenanceData>()
+                });
+            await sut.Handle(request, CancellationToken.None);
+
+            //Assert
+            ticketing.Verify(x =>
+                x.Error(
+                    It.IsAny<Guid>(),
+                    new TicketError(
+                        "Deze actie is enkel toegestaan binnen straatnamen met status 'voorgesteld' of 'inGebruik'.",
+                        "AdresStraatnaamVoorgesteldOfInGebruik"),
+                    CancellationToken.None));
+        }
+    }
+}

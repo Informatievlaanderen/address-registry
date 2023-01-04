47aa97c154de97d1a7c4d379b43bc806b36892f7
pgallik <patrickgallik@gmail.com>
2023-01-04 16:09:46 +0100

    feat: expand index on syndication item AddressBuildingUnitLinksExtract

--- src/AddressRegistry.Projections.Syndication/Migrations/20230104144353_AddIndexOnAddressBuildingUnitLinksExtractWithIncludeBuildingUnitPersistentLocalId.Designer.cs
+++ src/AddressRegistry.Projections.Syndication/Migrations/20230104144353_AddIndexOnAddressBuildingUnitLinksExtractWithIncludeBuildingUnitPersistentLocalId.Designer.cs
@@ -4,6 +4,7 @@ using AddressRegistry.Projections.Syndication;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AddressRegistry.Projections.Syndication.Migrations
 {
     [DbContext(typeof(SyndicationContext))]
-    partial class SyndicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230104144353_AddIndexOnAddressBuildingUnitLinksExtractWithIncludeBuildingUnitPersistentLocalId")]
+    partial class AddIndexOnAddressBuildingUnitLinksExtractWithIncludeBuildingUnitPersistentLocalId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

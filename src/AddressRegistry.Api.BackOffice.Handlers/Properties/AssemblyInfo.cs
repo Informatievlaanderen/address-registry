4c0dc8a850725a06f0a3a83ccbc724c66aea5809
jvandaal <jonas.vandaal@outlook.be>
2023-01-30 08:34:52 +0100

    refactor: remove handlers project and related tests

--- src/AddressRegistry.Api.BackOffice.Handlers/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyDescription("AddressRegistry BackOffice Handlers")]
-
-[assembly: ComVisible(false)]
-[assembly: Guid("491f6b24-f1d8-4377-9a24-ed11bfa537a2")]

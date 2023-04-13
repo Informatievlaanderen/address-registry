80088f7a2b7b7fbe2cf8006bccb90dbc012d3c96
Arne Dumarey <dumarey.arne@gmail.com>
2023-04-13 12:37:44 +0200

    refactor: rename extract addresslinks files

--- src/AddressRegistry.Api.Extract/Extracts/ExtractFileNames.cs
+++ src/AddressRegistry.Api.Extract/Extracts/ExtractFileNames.cs
@@ -7,8 +7,8 @@ namespace AddressRegistry.Api.Extract.Extracts
         public const string Address = "Adres";
         public const string CrabHouseNumberId = "CrabHuisnummer";
         public const string CrabSubadresId = "CrabSubadres";
-        public const string BuildingUnitLinks = "Adreskoppelingen";
-        public const string ParcelLinks = "Adreskoppelingen_1";
+        public const string BuildingUnitLinks = "AdresGebouweenheidKoppelingen";
+        public const string ParcelLinks = "AdresPerceelKoppelingen";
 
         public static string GetAddressZip() => $"Adres-{DateTime.Today:yyyy-MM-dd}";
         public static string GetAddressCrabZip() => $"Adres-Crab-{DateTime.Today:yyyy-MM-dd}";

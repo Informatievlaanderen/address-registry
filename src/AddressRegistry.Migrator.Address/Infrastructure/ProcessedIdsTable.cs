99555bf66eff38af34fb31bff9a6cb25606328b1
Arne Dumarey <dumarey.arne@gmail.com>
2023-08-29 09:40:24 +0200

    fix(migrator): add retry logic when parent not found

--- src/AddressRegistry.Migrator.Address/Infrastructure/ProcessedIdsTable.cs
+++ src/AddressRegistry.Migrator.Address/Infrastructure/ProcessedIdsTable.cs
@@ -15,7 +15,9 @@ namespace AddressRegistry.Migrator.Address.Infrastructure
         private readonly ILogger<ProcessedIdsTable> _logger;
 
         private const string ProcessedIdsTableName = "ProcessedIds";
+        private const string ProcessedIdsToRetryTableName = "RetryProcessedIds";
         private const string Table = $"[{Schema.MigrateAddress}].[{ProcessedIdsTableName}]";
+        private const string RetryTable = $"[{Schema.MigrateAddress}].[{ProcessedIdsToRetryTableName}]";
 
         public ProcessedIdsTable(string connectionString, ILoggerFactory loggerFactory)
         {
@@ -36,10 +38,21 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='{ProcessedIdsTableName}' and
 CREATE TABLE {Table}(
 [Id] [int] NOT NULL,
 [IsPageCompleted] [bit] NOT NULL DEFAULT 0,
-CONSTRAINT [PK_ProcessedIds] PRIMARY KEY CLUSTERED 
+CONSTRAINT [PK_ProcessedIds] PRIMARY KEY CLUSTERED
 (
 	[Id] ASC
-))");
+))
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='{ProcessedIdsToRetryTableName}' and xtype='U')
+CREATE TABLE {RetryTable}(
+[Id] [int] NOT NULL,
+[AggregateId] [nvarchar](50) NOT NULL,
+[IsPageCompleted] [bit] NOT NULL DEFAULT 0,
+CONSTRAINT [PK_RetryProcessedIds] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+))
+");
         }
 
         public async Task Add(int internalId)
@@ -51,7 +64,21 @@ CONSTRAINT [PK_ProcessedIds] PRIMARY KEY CLUSTERED
             }
             catch(Exception ex)
             {
-                _logger.LogCritical(ex, $"Failed to add Id '{internalId}' to ProcessedIds table");
+                _logger.LogCritical(ex, $"Failed to add Id '{internalId}' to {Table} table");
+                throw;
+            }
+        }
+
+        public async Task AddRetry(int internalId, string aggregateId)
+        {
+            try
+            {
+                await using var conn = new SqlConnection(_connectionString);
+                await conn.ExecuteAsync(@$"INSERT INTO {RetryTable} (Id, AggregateId, IsPageCompleted) VALUES ('{internalId}', '{aggregateId}', 0)");
+            }
+            catch(Exception ex)
+            {
+                _logger.LogCritical(ex, $"Failed to add Id '{internalId}' to {RetryTable} table");
                 throw;
             }
         }
@@ -65,10 +92,26 @@ CONSTRAINT [PK_ProcessedIds] PRIMARY KEY CLUSTERED
             await conn.ExecuteAsync(query, new { processedIds = processedIds.ToArray() });
         }
 
+        public async Task CompleteRetryPageAsync (IEnumerable<int> processedIds)
+        {
+            string query = $"UPDATE {RetryTable} SET IsPageCompleted = 1 WHERE Id IN @processedIds;";
+
+            await using var conn = new SqlConnection(_connectionString);
+
+            await conn.ExecuteAsync(query, new { processedIds = processedIds.ToArray() });
+        }
+
         public async Task<IEnumerable<(int processedId, bool isPageCompleted)>> GetProcessedIds()
         {
             await using var conn = new SqlConnection(_connectionString);
             var result = await conn.QueryAsync<(int, bool)>($"SELECT Id, IsPageCompleted FROM {Table} ORDER BY Id desc");
+            return result;
+        }
+
+        public async Task<IEnumerable<(int processedId, string aggregateId, bool isPageCompleted)>> GetRetryProcessedIds()
+        {
+            await using var conn = new SqlConnection(_connectionString);
+            var result = await conn.QueryAsync<(int, string, bool)>($"SELECT Id, AggregateId, IsPageCompleted FROM {RetryTable} ORDER BY Id desc");
             return result;
         }
     }

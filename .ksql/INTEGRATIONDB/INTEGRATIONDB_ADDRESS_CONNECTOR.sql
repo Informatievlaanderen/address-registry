65064ecbc1cb1d4ce78f754a9da371120d861682
pgallik <patrickgallik@gmail.com>
2023-11-29 15:19:31 +0100

    feat: add integrationdb ksql scripts

---
+++ .ksql/INTEGRATIONDB/INTEGRATIONDB_ADDRESS_CONNECTOR.sql
@@ -0,0 +1,28 @@
+CREATE SINK CONNECTOR `AddressIntegrationDbConnector` with (
+    "topics"= 'address.snapshot.oslo.flatten.integrationdb',
+    "input.data.format"= 'JSON_SR',
+    "input.key.format"= 'JSON_SR',
+    "delete.enabled"= false,
+    "connector.class"= 'PostgresSink',
+    "name"= 'AddressIntegrationDbConnector',
+    "kafka.auth.mode"= 'KAFKA_API_KEY',
+    "kafka.api.key"= '***', --clear value
+    "kafka.api.secret"= '***', --clear value
+    "connection.host"= '***', --clear value
+    "connection.port"= '5432',
+    "connection.user"= '***', --clear value
+    "connection.password"= '***', --clear value
+    "db.name"= 'postgres',
+    "ssl.mode"= 'require',
+    "insert.mode"= 'UPSERT',
+    "table.name.format"= 'Integration.Address',
+    "table.types"= 'TABLE',
+    "db.timezone"= 'UTC',
+    "pk.mode"= 'record_key',
+    "pk.fields"= 'PersistentLocalId',
+    "auto.create"= false,
+    "auto.evolve"= false,
+    "quote.sql.identifiers"= 'ALWAYS',
+    "batch.sizes"= 3000,
+    "tasks.max"= 1
+  );

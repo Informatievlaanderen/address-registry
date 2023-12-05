fdbe6b740a241b678384c49fd269420bc01a6688
jvandaal <jonas.vandaal@outlook.be>
2023-12-05 17:20:30 +0100

    refactor: remove api.extract legacy code + toggle

--- src/AddressRegistry.Api.Extract/Extracts/ExtractController.cs
+++ src/AddressRegistry.Api.Extract/Extracts/ExtractController.cs
@@ -1,20 +1,19 @@
 namespace AddressRegistry.Api.Extract.Extracts
 {
-    using Be.Vlaanderen.Basisregisters.Api;
-    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Mvc;
-    using Projections.Extract;
-    using Responses;
-    using Swashbuckle.AspNetCore.Filters;
     using System.Threading;
     using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Api;
+    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
     using Be.Vlaanderen.Basisregisters.Api.Extract;
     using Consumer.Read.Municipality;
     using Consumer.Read.StreetName;
-    using Infrastructure.FeatureToggles;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Configuration;
+    using Projections.Extract;
     using Projections.Syndication;
+    using Responses;
+    using Swashbuckle.AspNetCore.Filters;
     using ProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ProblemDetails;
 
     [ApiVersion("1.0")]
@@ -27,7 +26,6 @@ namespace AddressRegistry.Api.Extract.Extracts
         /// Vraag een dump van het volledige register op.
         /// </summary>
         /// <param name="context"></param>
-        /// <param name="useExtractV2Toggle"></param>
         /// <param name="syndicationContext"></param>
         /// <param name="municipalityConsumerContext"></param>
         /// <param name="streetNameConsumerContext"></param>
@@ -41,24 +39,14 @@ namespace AddressRegistry.Api.Extract.Extracts
         [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
         public async Task<IActionResult> Get(
             [FromServices] ExtractContext context,
-            [FromServices] UseExtractV2Toggle useExtractV2Toggle,
             [FromServices] SyndicationContext syndicationContext,
             [FromServices] MunicipalityConsumerContext municipalityConsumerContext,
             [FromServices] StreetNameConsumerContext streetNameConsumerContext,
             CancellationToken cancellationToken = default)
         {
-            if (useExtractV2Toggle.FeatureEnabled)
-            {
-                return new IsolationExtractArchive(ExtractFileNames.GetAddressZip(), context)
-                    {
-                        AddressRegistryExtractBuilder.CreateAddressFilesV2(context, streetNameConsumerContext, municipalityConsumerContext)
-                    }
-                    .CreateFileCallbackResult(cancellationToken);
-            }
-
             return new IsolationExtractArchive(ExtractFileNames.GetAddressZip(), context)
                 {
-                    AddressRegistryExtractBuilder.CreateAddressFiles(context, syndicationContext)
+                    AddressRegistryExtractBuilder.CreateAddressFilesV2(context, streetNameConsumerContext, municipalityConsumerContext)
                 }
                 .CreateFileCallbackResult(cancellationToken);
         }

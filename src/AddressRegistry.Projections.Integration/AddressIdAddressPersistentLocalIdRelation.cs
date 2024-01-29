b5bfcfbb88f4f34c0f5e826cfee41b5ea14c1a22
emalfroy <egon.malfroy@gmail.com>
2024-01-29 09:42:54 +0100

    feat: add address id persistentlocalid relation projections integrationdb

---
+++ src/AddressRegistry.Projections.Integration/AddressIdAddressPersistentLocalIdRelation.cs
@@ -0,0 +1,36 @@
+﻿namespace AddressRegistry.Projections.Integration
+{
+    using System;
+    using AddressRegistry.Infrastructure;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class AddressIdAddressPersistentLocalIdRelation
+    {
+        public Guid AddressId { get; set; }
+
+        public int PersistentLocalId { get; set; }
+
+        public AddressIdAddressPersistentLocalIdRelation()
+        { }
+    }
+
+    public sealed class AddressIdAddressPersistentLocalIdConfiguration : IEntityTypeConfiguration<AddressIdAddressPersistentLocalIdRelation>
+    {
+        public void Configure(EntityTypeBuilder<AddressIdAddressPersistentLocalIdRelation> builder)
+        {
+            const string tableName = "address_id_address_persistent_local_id";
+
+            builder
+                .ToTable(tableName, Schema.Integration) // to schema per type
+                .HasKey(x => new { x.AddressId });
+
+
+            builder.Property(x => x.AddressId).HasColumnName("address_id");
+            builder.Property(x => x.PersistentLocalId).HasColumnName("persistent_local_id");
+
+            builder.HasIndex(x => x.PersistentLocalId);
+            builder.HasIndex(x => x.AddressId);
+        }
+    }
+}
--- src/AddressRegistry.Projections.Integration/EventsRepository.cs
+++ src/AddressRegistry.Projections.Integration/EventsRepository.cs
@@ -8,22 +8,22 @@
     public class EventsRepository : IEventsRepository
 
     {
-    private readonly string _eventsConnectionString;
+        private readonly string _eventsConnectionString;
 
-    public EventsRepository(string eventsConnectionString)
-    {
-        _eventsConnectionString = eventsConnectionString;
-    }
+        public EventsRepository(string eventsConnectionString)
+        {
+            _eventsConnectionString = eventsConnectionString;
+        }
 
-    public async Task<int?> GetAddressPersistentLocalId(Guid addressId)
-    {
-        await using var connection = new SqlConnection(_eventsConnectionString);
-        var sql = @$"SELECT Json_Value(JsonData, '$.persistentLocalId') AS ""AddressPersistentLocalId""
+        public async Task<int?> GetAddressPersistentLocalId(Guid addressId)
+        {
+            await using var connection = new SqlConnection(_eventsConnectionString);
+            var sql = @$"SELECT Json_Value(JsonData, '$.persistentLocalId') AS ""AddressPersistentLocalId""
                     FROM [address-registry-events].[AddressRegistry].[Streams] as s
                     inner join [address-registry-events].[AddressRegistry].[Messages] as m on s.IdInternal = m.StreamIdInternal and m.[Type] = 'AddressPersistentLocalIdentifierWasAssigned'
                     where s.Id = '{addressId}'";
 
-        return await connection.QuerySingleOrDefaultAsync<int?>(sql);
-    }
+            return await connection.QuerySingleOrDefaultAsync<int?>(sql);
+        }
     }
 }

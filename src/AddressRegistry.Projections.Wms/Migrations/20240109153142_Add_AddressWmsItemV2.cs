c1b209f5af5781bb2ca65f21b3a5432412c63788
jvandaal <jonas.vandaal@outlook.be>
2024-02-01 16:13:23 +0100

    fix: add spatial index on AddressWmsV2

--- src/AddressRegistry.Projections.Wms/Migrations/20240109153142_Add_AddressWmsItemV2.cs
+++ src/AddressRegistry.Projections.Wms/Migrations/20240109153142_Add_AddressWmsItemV2.cs
@@ -6,6 +6,8 @@ using NetTopologySuite.Geometries;
 
 namespace AddressRegistry.Projections.Wms.Migrations
 {
+    using Infrastructure;
+
     public partial class Add_AddressWmsItemV2 : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -40,6 +42,19 @@ namespace AddressRegistry.Projections.Wms.Migrations
                     table.PrimaryKey("PK_AddressWmsV2", x => x.AddressPersistentLocalId)
                         .Annotation("SqlServer:Clustered", true);
                 });
+
+            migrationBuilder.Sql(
+                $"CREATE SPATIAL INDEX [SPATIAL_Address_Position] ON [{Schema.Wms}].[AddressWmsV2] ([Position])\n" +
+                @"USING  GEOMETRY_GRID
+	                WITH (
+		                BOUNDING_BOX =(22279.17, 153050.23, 258873.3, 244022.31),
+		                GRIDS =(
+			                LEVEL_1 = MEDIUM,
+			                LEVEL_2 = MEDIUM,
+			                LEVEL_3 = MEDIUM,
+			                LEVEL_4 = MEDIUM),
+	                CELLS_PER_OBJECT = 5)
+	                GO");
 
             migrationBuilder.CreateIndex(
                 name: "IX_AddressWmsV2_ParentAddressPersistentLocalId",

c27f5b64780c93de339aa3a77830bd2b97069a7b
Arne Dumarey <dumarey.arne@gmail.com>
2024-02-02 10:04:00 +0100

    feat: add cachevalidator lastchangedlist GAWR-5407

---
+++ src/AddressRegistry.Projector/Infrastructure/LastChangedListCacheValidator.cs
@@ -0,0 +1,33 @@
+﻿namespace AddressRegistry.Projector.Infrastructure
+{
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AddressRegistry.Projections.Legacy;
+    using Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList;
+    using Microsoft.EntityFrameworkCore;
+
+    public sealed class LastChangedListCacheValidator : ICacheValidator
+    {
+        private readonly LegacyContext _legacyContext;
+        private readonly string _projectionName;
+
+        public LastChangedListCacheValidator(LegacyContext legacyContext, string projectionName)
+        {
+            _legacyContext = legacyContext;
+            _projectionName = projectionName;
+        }
+
+        public async Task<bool> CanCache(long position, CancellationToken ct)
+        {
+            var projectionPosition = await _legacyContext
+                .ProjectionStates
+                .AsNoTracking()
+                .Where(ps => ps.Name == _projectionName)
+                .Select(ps => ps.Position)
+                .FirstOrDefaultAsync(ct);
+
+            return projectionPosition >= position;
+        }
+    }
+}

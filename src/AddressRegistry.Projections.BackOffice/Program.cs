bd194dd3a62f7b5eb8605aa43baf387ba5db8d60
jvandaal <jonas.vandaal@outlook.be>
2024-02-19 15:29:53 +0100

    feat: shutdown backoffice projections service when projection is stopped

--- src/AddressRegistry.Projections.BackOffice/Program.cs
+++ src/AddressRegistry.Projections.BackOffice/Program.cs
@@ -87,7 +87,8 @@ namespace AddressRegistry.Projections.BackOffice
                                 .EnableRetryOnFailure()
                                 .MigrationsHistoryTable(MigrationTables.BackOffice, Schema.BackOffice)
                             ))
-                        .AddHostedService<ProjectorRunner>();
+                        .AddHostedService<ProjectorRunner>()
+                        .AddHostedService<ProjectionsHealthCheckRunner>();
                 })
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureContainer<ContainerBuilder>((hostContext, builder) =>

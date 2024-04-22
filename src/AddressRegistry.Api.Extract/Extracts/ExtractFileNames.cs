1be57cdc1352b2d713d66f8e9b3645ef990083cb
Arne Dumarey <dumarey.arne@gmail.com>
2024-04-22 09:28:00 +0200

    feat: remove crab from extract GAWR-4677

--- src/AddressRegistry.Api.Extract/Extracts/ExtractFileNames.cs
+++ src/AddressRegistry.Api.Extract/Extracts/ExtractFileNames.cs
@@ -5,13 +5,10 @@ namespace AddressRegistry.Api.Extract.Extracts
     internal static class ExtractFileNames
     {
         public const string Address = "Adres";
-        public const string CrabHouseNumberId = "CrabHuisnummer";
-        public const string CrabSubadresId = "CrabSubadres";
         public const string BuildingUnitLinks = "AdresGebouweenheidKoppelingen";
         public const string ParcelLinks = "AdresPerceelKoppelingen";
 
         public static string GetAddressZip() => $"Adres-{DateTime.Today:yyyy-MM-dd}";
-        public static string GetAddressCrabZip() => $"Adres-Crab-{DateTime.Today:yyyy-MM-dd}";
         public static string GetAddressLinksZip() => $"Adreskoppelingen-{DateTime.Today:yyyy-MM-dd}";
     }
 }

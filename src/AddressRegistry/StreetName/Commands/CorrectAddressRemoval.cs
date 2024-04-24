369a7f195f88eb7ed4a6deb836cbf224ae634e3a
jvandaal <jonas.vandaal@outlook.be>
2024-04-24 11:49:09 +0200

    feat: add correct address removal event + command

---
+++ src/AddressRegistry/StreetName/Commands/CorrectAddressRemoval.cs
@@ -0,0 +1,44 @@
+namespace AddressRegistry.StreetName.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using Be.Vlaanderen.Basisregisters.Generators.Guid;
+    using Be.Vlaanderen.Basisregisters.GrAr.Provenance;
+    using Be.Vlaanderen.Basisregisters.Utilities;
+
+    public class CorrectAddressRemoval : IHasCommandProvenance
+    {
+        private static readonly Guid Namespace = new Guid("538b2703-69db-4c33-ae2c-101703926eb5");
+
+        public StreetNamePersistentLocalId StreetNamePersistentLocalId { get; }
+        public AddressPersistentLocalId AddressPersistentLocalId { get; }
+        public Provenance Provenance { get; }
+
+        public CorrectAddressRemoval(
+            StreetNamePersistentLocalId streetNamePersistentLocalId,
+            AddressPersistentLocalId addressPersistentLocalId,
+            Provenance provenance)
+        {
+            StreetNamePersistentLocalId = streetNamePersistentLocalId;
+            AddressPersistentLocalId = addressPersistentLocalId;
+            Provenance = provenance;
+        }
+
+        public Guid CreateCommandId()
+            => Deterministic.Create(Namespace, $"CorrectAddressRemoval-{ToString()}");
+
+        public override string? ToString()
+            => ToStringBuilder.ToString(IdentityFields());
+
+        private IEnumerable<object> IdentityFields()
+        {
+            yield return StreetNamePersistentLocalId;
+            yield return AddressPersistentLocalId;
+
+            foreach (var field in Provenance.GetIdentityFields())
+            {
+                yield return field;
+            }
+        }
+    }
+}

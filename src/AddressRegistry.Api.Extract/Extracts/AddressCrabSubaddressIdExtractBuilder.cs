d249220f912a22a3b233bbb3c2f840a577faf450
ArneD <dumarey.arne@gmail.com>
2024-04-30 10:50:51 +0200

    feat: revert remove crab from extract GAWR-4677
    This reverts commit 1be57cdc1352b2d713d66f8e9b3645ef990083cb.

---
+++ src/AddressRegistry.Api.Extract/Extracts/AddressCrabSubaddressIdExtractBuilder.cs
@@ -0,0 +1,27 @@
+namespace AddressRegistry.Api.Extract.Extracts
+{
+    using System.Linq;
+    using Be.Vlaanderen.Basisregisters.Api.Extract;
+    using Be.Vlaanderen.Basisregisters.GrAr.Extracts;
+    using Microsoft.EntityFrameworkCore;
+    using Projections.Extract;
+    using Projections.Extract.AddressCrabSubaddressIdExtract;
+
+    public static class AddressCrabSubaddressIdExtractBuilder
+    {
+        public static ExtractFile CreateAddressSubaddressIdFile(ExtractContext context)
+        {
+            var extractItems = context
+               .AddressCrabSubaddressIdExtract
+               .AsNoTracking()
+               .Where(m => m.CrabSubaddressId.HasValue && m.PersistentLocalId.HasValue)
+               .OrderBy(m => m.PersistentLocalId);
+
+            return ExtractBuilder.CreateDbfFile<AddressCrabSubaddressIdDbaseRecord>(
+                ExtractFileNames.CrabSubadresId,
+                new AddressCrabSubaddressIdDbaseSchema(),
+                extractItems.Select(x => x.DbaseRecord!),
+                extractItems.Count);
+        }
+    }
+}
--- src/AddressRegistry.Api.Extract/Extracts/ExtractFileNames.cs
+++ src/AddressRegistry.Api.Extract/Extracts/ExtractFileNames.cs
@@ -6,8 +6,11 @@ namespace AddressRegistry.Api.Extract.Extracts
     {
         public const string Address = "Adres";
         public const string PostalCodeStreetNameLinks = "PostcodeStraatnaamKoppelingen";
+        public const string CrabHouseNumberId = "CrabHuisnummer";
+        public const string CrabSubadresId = "CrabSubadres";
 
         public static string GetAddressZip() => $"{Address}-{DateTime.Today:yyyy-MM-dd}";
         public static string GetPostalCodeStreetNameLinksZip() => $"{PostalCodeStreetNameLinks}-{DateTime.Today:yyyy-MM-dd}";
+        public static string GetAddressCrabZip() => $"Adres-Crab-{DateTime.Today:yyyy-MM-dd}";
     }
 }

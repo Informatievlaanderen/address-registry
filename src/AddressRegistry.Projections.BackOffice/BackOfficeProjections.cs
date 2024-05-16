1575b6e4a59998da284f9240dd0748e5a13c4e7c
Arne Dumarey <dumarey.arne@gmail.com>
2024-05-16 16:09:55 +0200

    fix: add delay to backoffice projections to avoid conflict with lambda handler

--- src/AddressRegistry.Projections.BackOffice/BackOfficeProjections.cs
+++ src/AddressRegistry.Projections.BackOffice/BackOfficeProjections.cs
@@ -1,17 +1,25 @@
 namespace AddressRegistry.Projections.BackOffice
 {
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Api.BackOffice.Abstractions;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.Connector;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.SqlStreamStore;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
     using StreetName.Events;
 
     public sealed class BackOfficeProjections : ConnectedProjection<BackOfficeProjectionsContext>
     {
-        public BackOfficeProjections(IDbContextFactory<BackOfficeContext> backOfficeContextFactory)
+        public BackOfficeProjections(IDbContextFactory<BackOfficeContext> backOfficeContextFactory, IConfiguration configuration)
         {
+            var delayInSeconds = configuration.GetValue<int>("DelayInSeconds", 10);
+
             When<Envelope<AddressWasProposedV2>>(async (_, message, cancellationToken) =>
             {
+                await DelayProjection(message.CreatedUtc, delayInSeconds, cancellationToken);
+
                 await using var backOfficeContext = await backOfficeContextFactory.CreateDbContextAsync(cancellationToken);
                 await backOfficeContext.AddIdempotentAddressStreetNameIdRelation(
                     message.Message.AddressPersistentLocalId, message.Message.StreetNamePersistentLocalId, cancellationToken);
@@ -19,10 +27,21 @@ namespace AddressRegistry.Projections.BackOffice
 
             When<Envelope<AddressWasProposedBecauseOfReaddress>>(async (_, message, cancellationToken) =>
             {
+                await DelayProjection(message.CreatedUtc, delayInSeconds, cancellationToken);
+
                 await using var backOfficeContext = await backOfficeContextFactory.CreateDbContextAsync(cancellationToken);
                 await backOfficeContext.AddIdempotentAddressStreetNameIdRelation(
                     message.Message.AddressPersistentLocalId, message.Message.StreetNamePersistentLocalId, cancellationToken);
             });
+        }
+
+        private static async Task DelayProjection(DateTime messageTimestamp, int delayInSeconds, CancellationToken cancellationToken)
+        {
+            var differenceInSeconds = (DateTime.UtcNow -messageTimestamp).TotalSeconds;
+            if (differenceInSeconds < delayInSeconds)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(delayInSeconds - differenceInSeconds), cancellationToken);
+            }
         }
     }
 }

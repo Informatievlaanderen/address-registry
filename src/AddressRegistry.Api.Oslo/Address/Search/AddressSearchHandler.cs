a2e5c1229f4cba682f3383a0d81449b43ae735c4
Arne Dumarey <dumarey.arne@gmail.com>
2024-08-21 17:25:38 +0200

    feat: add search for streetname

---
+++ src/AddressRegistry.Api.Oslo/Address/Search/AddressSearchHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace AddressRegistry.Api.Oslo.Address.Search
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Infrastructure.Options;
+    using MediatR;
+    using Microsoft.Extensions.Options;
+
+    public sealed class AddressSearchHandler : IRequestHandler<AddressSearchRequest, AddressSearchResponse>
+    {
+        private readonly ResponseOptions _responseOptions;
+
+        public AddressSearchHandler(IOptions<ResponseOptions> responseOptions)
+        {
+            _responseOptions = responseOptions.Value;
+        }
+
+        public async Task<AddressSearchResponse> Handle(AddressSearchRequest request, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(request.Filtering.Filter.Query))
+                return new AddressSearchResponse(new List<AddressSearchItem>());
+
+            return new AddressSearchResponse(new List<AddressSearchItem>());
+        }
+    }
+}
---
+++ src/AddressRegistry.Api.Oslo/Address/Search/AddressSearchRequest.cs
@@ -0,0 +1,54 @@
+﻿namespace AddressRegistry.Api.Oslo.Address.Search
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+    using Be.Vlaanderen.Basisregisters.Api.Search.Filtering;
+    using Be.Vlaanderen.Basisregisters.Api.Search.Pagination;
+    using Be.Vlaanderen.Basisregisters.Api.Search.Sorting;
+    using MediatR;
+    using Newtonsoft.Json;
+
+    public sealed record AddressSearchRequest(
+        FilteringHeader<AddressSearchFilter> Filtering,
+        SortingHeader Sorting,
+        IPaginationRequest Pagination) : IRequest<AddressSearchResponse>;
+
+    public sealed record AddressSearchResponse(IEnumerable<AddressSearchItem> Items);
+
+    public sealed class AddressSearchItem
+    {
+        /// <summary>
+        /// De unieke en persistente identificator van het object (volgt de Vlaamse URI-standaard).
+        /// </summary>
+        [DataMember(Name = "Id", Order = 1)]
+        [JsonProperty(Required = Required.DisallowNull)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// De objectidentificator (enkel uniek binnen naamruimte).
+        /// </summary>
+        [DataMember(Name = "ObjectId", Order = 2)]
+        [JsonProperty(Required = Required.DisallowNull)]
+        public string ObjectId { get; set; }
+
+        /// <summary>
+        /// De URL die naar de details van de meeste recente versie van een enkel adres leidt.
+        /// </summary>
+        [DataMember(Name = "Detail", Order = 3)]
+        [JsonProperty(Required = Required.DisallowNull)]
+        public Uri Detail { get; set; }
+
+        /// <summary>
+        /// Textueel resultaat van de zoekopdracht (straatnaam of volledig adres).
+        /// </summary>
+        [DataMember(Name = "Resultaat", Order = 4)]
+        [JsonProperty(Required = Required.DisallowNull)]
+        public string Result { get; set; }
+    }
+
+    public sealed class AddressSearchFilter
+    {
+        public string Query { get; init; }
+    }
+}

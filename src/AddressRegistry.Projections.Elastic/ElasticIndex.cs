fe899a953ceb4fe2e468c616f50e7b412e5bc50e
Rik De Peuter <rikdepeuter@gmail.com>
2024-08-28 14:47:32 +0200

    fix: elastic index settings (sort+max results)

--- src/AddressRegistry.Projections.Elastic/ElasticIndex.cs
+++ src/AddressRegistry.Projections.Elastic/ElasticIndex.cs
@@ -58,10 +58,12 @@
 
             var createResponse = await _client.Indices.CreateAsync<AddressSearchDocument>(indexName, c =>
             {
-                c.Settings(x => x.MaxResultWindow(1_000_500)); // Linked to public-api offset of 1_000_000 + limit of 500
-
-                c.Settings(x => x.Analysis(a =>
-                    a
+                c.Settings(x => x
+                    .MaxResultWindow(1_000_500) // Linked to public-api offset of 1_000_000 + limit of 500
+                    .Sort(s => s
+                        .Field(Fields.FromExpression((AddressSearchDocument d) => d.AddressPersistentLocalId))
+                        .Order([SegmentSortOrder.Asc]))
+                    .Analysis(a => a
                         .CharFilters(cf => cf
                             .PatternReplace("dot_replace", prcf => prcf.Pattern("\\.").Replacement(""))
                             .PatternReplace("underscore_replace", prcf => prcf.Pattern("_").Replacement(" ")))

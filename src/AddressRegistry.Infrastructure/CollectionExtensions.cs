9b864c9113ebc5bc460f26037d1c12ea017f3a26
jvandaal <jonas.vandaal@outlook.be>
2024-10-23 14:03:37 +0200

    fix: batch too large sqs requests

---
+++ src/AddressRegistry.Infrastructure/CollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace AddressRegistry.Infrastructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class CollectionExtensions
+    {
+        public static IEnumerable<ICollection<T>> SplitBySize<T>(this ICollection<T> collection, int batchSize)
+        {
+            var batchCount = (double)collection.Count / batchSize;
+
+            var batchCountRounded = (int)Math.Floor(batchCount);
+            if (batchCount != batchCountRounded)
+            {
+                batchCountRounded++;
+            }
+
+            return Enumerable.Range(0, batchCountRounded)
+                .Select(batchIndex => collection.Skip(batchSize * batchIndex).Take(batchSize).ToArray());
+        }
+    }
+}

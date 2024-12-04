097e584cb2d62dae505963e19529b4802a6219d4
jvandaal <jonas.vandaal@outlook.be>
2024-12-04 11:24:00 +0100

    feat: configure consumer offset without deploy

---
+++ src/AddressRegistry.Producer/Migrations/20241204102123_AddOffsetOverride.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AddressRegistry.Producer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOffsetOverride : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProjectionStates",
+                schema: "AddressRegistryProducer",
+                table: "ProjectionStates");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProjectionStates",
+                schema: "AddressRegistryProducer",
+                table: "ProjectionStates",
+                column: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProjectionStates",
+                schema: "AddressRegistryProducer",
+                table: "ProjectionStates");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProjectionStates",
+                schema: "AddressRegistryProducer",
+                table: "ProjectionStates",
+                column: "Name")
+                .Annotation("SqlServer:Clustered", true);
+        }
+    }
+}

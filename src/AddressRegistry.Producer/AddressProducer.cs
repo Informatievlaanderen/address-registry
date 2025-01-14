3e7282cc052e4a9bff644a8d64ea16e47e3d35fd
Arne Dumarey <dumarey.arne@gmail.com>
2025-01-14 13:35:51 +0100

    refactor(producers): convert to consoleapp

---
+++ src/AddressRegistry.Producer/AddressProducer.cs
@@ -0,0 +1,40 @@
+﻿namespace AddressRegistry.Producer
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Projector.ConnectedProjections;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    public sealed class AddressProducer : BackgroundService
+    {
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+        private readonly ILogger<AddressProducer> _logger;
+        private readonly IConnectedProjectionsManager _projectionManager;
+
+        public AddressProducer(
+            IHostApplicationLifetime hostApplicationLifetime,
+            IConnectedProjectionsManager projectionManager,
+            ILogger<AddressProducer> logger)
+        {
+            _hostApplicationLifetime = hostApplicationLifetime;
+            _projectionManager = projectionManager;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                await _projectionManager.Start(stoppingToken);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogCritical(exception, $"Critical error occured in {nameof(AddressProducer)}.");
+                _hostApplicationLifetime.StopApplication();
+                throw;
+            }
+        }
+    }
+}
--- src/AddressRegistry.Producer/Infrastructure/Program.cs
+++ src/AddressRegistry.Producer/Infrastructure/Program.cs
@@ -1,40 +1,158 @@
 namespace AddressRegistry.Producer.Infrastructure
 {
-    using Be.Vlaanderen.Basisregisters.Api;
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Autofac;
+    using Autofac.Extensions.DependencyInjection;
     using Be.Vlaanderen.Basisregisters.Aws.DistributedMutex;
+    using Destructurama;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+    using Modules;
+    using Serilog;
+    using Serilog.Debugging;
+    using Serilog.Extensions.Logging;
 
-    public class Program
+    public sealed class Program
     {
-        protected Program()
+        private Program()
         { }
-        
-        public static void Main(string[] args)
-            => Run(new ProgramOptions
-                {
-                    Hosting =
-                    {
-                        HttpPort = 5014
-                    },
-                    Logging =
-                    {
-                        WriteTextToConsole = false,
-                        WriteJsonToConsole = false
-                    },
-                    Runtime =
-                    {
-                        CommandLineArgs = args
-                    },
-                    MiddlewareHooks =
-                    {
-                        ConfigureDistributedLock =
-                            configuration => DistributedLockOptions.LoadFromConfiguration(configuration)
-                    }
-                });
 
-        private static void Run(ProgramOptions options)
-            => new WebHostBuilder()
-                .UseDefaultForApi<Startup>(options)
-                .RunWithLock<Program>();
+        public static async Task Main(string[] args)
+        {
+            AppDomain.CurrentDomain.FirstChanceException += (_, eventArgs) =>
+                Log.Debug(
+                    eventArgs.Exception,
+                    "FirstChanceException event raised in {AppDomain}.",
+                    AppDomain.CurrentDomain.FriendlyName);
+
+            AppDomain.CurrentDomain.UnhandledException += (_, eventArgs) =>
+                Log.Fatal((Exception)eventArgs.ExceptionObject, "Encountered a fatal exception, exiting program.");
+
+            Log.Information("Initializing AddressRegistry.Producer");
+
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration((_, configurationBuilder) =>
+                {
+                    configurationBuilder
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+                        .AddJsonFile($"appsettings.{Environment.MachineName.ToLowerInvariant()}.json", optional: true, reloadOnChange: false)
+                        .AddEnvironmentVariables()
+                        .AddCommandLine(args);
+                })
+                .ConfigureLogging((hostContext, loggingBuilder) =>
+                {
+                    SelfLog.Enable(Console.WriteLine);
+
+                    Log.Logger = new LoggerConfiguration() //NOSONAR logging configuration is safe
+                        .ReadFrom.Configuration(hostContext.Configuration)
+                        .Enrich.FromLogContext()
+                        .Enrich.WithMachineName()
+                        .Enrich.WithThreadId()
+                        .Enrich.WithEnvironmentUserName()
+                        .Destructure.JsonNetTypes()
+                        .CreateLogger();
+
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.AddSerilog(Log.Logger);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var healthChecksBuilder = services.AddHealthChecks();
+                    var connectionStrings = hostContext.Configuration
+                        .GetSection("ConnectionStrings")
+                        .GetChildren();
+
+                    foreach (var connectionString in connectionStrings)
+                    {
+                        healthChecksBuilder.AddSqlServer(
+                            connectionString.Value,
+                            name: $"sqlserver-{connectionString.Key.ToLowerInvariant()}",
+                            tags: new[] { "db", "sql", "sqlserver" });
+                    }
+
+                    healthChecksBuilder.AddDbContextCheck<ProducerContext>(
+                        $"dbcontext-{nameof(ProducerContext).ToLowerInvariant()}",
+                        tags: new[] { "db", "sql", "sqlserver" });
+
+                    var origins = hostContext.Configuration
+                        .GetSection("Cors")
+                        .GetChildren()
+                        .Select(c => c.Value)
+                        .ToArray();
+
+                    foreach (var origin in origins)
+                    {
+                        services.AddCors(options =>
+                        {
+                            options.AddDefaultPolicy(builder =>
+                            {
+                                builder.WithOrigins(origin);
+                            });
+                        });
+                    }
+                })
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureContainer<ContainerBuilder>((hostContext, builder) =>
+                {
+                    var services = new ServiceCollection();
+                    var loggerFactory = new SerilogLoggerFactory(Log.Logger);
+
+                    builder.RegisterModule(new ProducerModule(hostContext.Configuration, services, loggerFactory));
+
+                    builder
+                        .RegisterType<AddressProducer>()
+                        .As<IHostedService>()
+                        .SingleInstance();
+
+                    builder.Populate(services);
+                })
+                .ConfigureWebHostDefaults(webHostBuilder =>
+                    webHostBuilder
+                        .UseStartup<Startup>()
+                        .UseKestrel())
+                .UseConsoleLifetime()
+                .Build();
+
+            Log.Information("Starting AddressRegistry.Producer");
+
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            var configuration = host.Services.GetRequiredService<IConfiguration>();
+
+            try
+            {
+                await DistributedLock<Program>.RunAsync(
+                        async () => { await host.RunAsync().ConfigureAwait(false); },
+                        DistributedLockOptions.LoadFromConfiguration(configuration),
+                        logger)
+                    .ConfigureAwait(false);
+            }
+            catch (AggregateException aggregateException)
+            {
+                foreach (var innerException in aggregateException.InnerExceptions)
+                {
+                    logger.LogCritical(innerException, "Encountered a fatal exception, exiting program.");
+                }
+            }
+            catch (Exception e)
+            {
+                logger.LogCritical(e, "Encountered a fatal exception, exiting program.");
+                Log.CloseAndFlush();
+
+                // Allow some time for flushing before shutdown.
+                await Task.Delay(500, default);
+                throw;
+            }
+            finally
+            {
+                logger.LogInformation("Stopping...");
+            }
+        }
     }
 }

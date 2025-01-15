96a373d0c6b3bd774ef8fca62c3d91aca712c00b
Arne Dumarey <dumarey.arne@gmail.com>
2025-01-15 13:53:38 +0100

    fix(wfs): computed column handle null labels

---
+++ src/AddressRegistry.Projections.Wfs/Migrations/20250115125249_FixComputedLabelLength.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AddressRegistry.Projections.Wfs.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixComputedLabelLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "HouseNumberLabelLength",
+                schema: "wfs.address",
+                table: "AddressWfsV2",
+                type: "int",
+                nullable: false,
+                computedColumnSql: "CAST(LEN(ISNULL(HouseNumberLabel, '')) AS INT)",
+                stored: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldComputedColumnSql: "LEN(HouseNumberLabel)",
+                oldStored: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "HouseNumberLabelLength",
+                schema: "wfs.address",
+                table: "AddressWfsV2",
+                type: "int",
+                nullable: false,
+                computedColumnSql: "LEN(HouseNumberLabel)",
+                stored: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldComputedColumnSql: "CAST(LEN(ISNULL(HouseNumberLabel, '')) AS INT)",
+                oldStored: true);
+        }
+    }
+}

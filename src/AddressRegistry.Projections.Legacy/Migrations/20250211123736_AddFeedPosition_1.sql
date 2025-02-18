b1b55addb9b2aa1514250091324a416e1a8442e4
Arne Dumarey <dumarey.arne@gmail.com>
2025-02-18 13:36:14 +0100

    chore(projections): add sql scripts to migrate syndication

---
+++ src/AddressRegistry.Projections.Legacy/Migrations/20250211123736_AddFeedPosition_1.sql
@@ -0,0 +1,95 @@
+﻿-- Part 1 to create table and filling up data
+CREATE TABLE [AddressRegistryLegacy].[AddressSyndication_2](
+  [Position] [bigint] NOT NULL,
+  [AddressId] [uniqueidentifier] NULL,
+  [PersistentLocalId] [int] NULL,
+  [ChangeType] [nvarchar](max) NULL,
+  [StreetNameId] [uniqueidentifier] NULL,
+  [PostalCode] [nvarchar](max) NULL,
+  [HouseNumber] [nvarchar](max) NULL,
+  [BoxNumber] [nvarchar](max) NULL,
+  [PointPosition] [varbinary](max) NULL,
+  [PositionMethod] [int] NULL,
+  [PositionSpecification] [int] NULL,
+  [Status] [int] NULL,
+  [IsComplete] [bit] NOT NULL,
+  [IsOfficiallyAssigned] [bit] NOT NULL,
+  [RecordCreatedAt] [datetimeoffset](7) NOT NULL,
+  [LastChangedOn] [datetimeoffset](7) NOT NULL,
+  [Application] [int] NULL,
+  [Modification] [int] NULL,
+  [Operator] [nvarchar](max) NULL,
+  [Organisation] [int] NULL,
+  [Reason] [nvarchar](max) NULL,
+  [EventDataAsXml] [nvarchar](max) NULL,
+  [SyndicationItemCreatedAt] [datetimeoffset](7) NOT NULL,
+  [StreetNamePersistentLocalId] [int] NULL,
+  CONSTRAINT [PK_AddressSyndication_2] PRIMARY KEY CLUSTERED
+(
+[Position] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+  ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+  GO
+
+-- NEW
+ALTER TABLE [AddressRegistryLegacy].[AddressSyndication_2] ADD [FeedPosition] bigint NOT NULL IDENTITY;
+GO
+
+SET IDENTITY_INSERT AddressRegistryLegacy.AddressSyndication_2 ON;
+GO
+
+ALTER TABLE [AddressRegistryLegacy].[AddressSyndication_2] DROP CONSTRAINT [PK_AddressSyndication_2];
+GO
+
+ALTER TABLE [AddressRegistryLegacy].[AddressSyndication_2] ADD CONSTRAINT [PK_AddressSyndication_2] PRIMARY KEY CLUSTERED ([FeedPosition]);
+GO
+
+truncate table[AddressRegistryLegacy].[AddressSyndication_2];
+GO
+
+INSERT INTO [AddressRegistryLegacy].[AddressSyndication_2]
+           ([Position]
+           ,[AddressId]
+           ,[PersistentLocalId]
+           ,[ChangeType]
+           ,[StreetNameId]
+           ,[PostalCode]
+           ,[HouseNumber]
+           ,[BoxNumber]
+           ,[PointPosition]
+           ,[PositionMethod]
+           ,[PositionSpecification]
+           ,[Status]
+           ,[IsComplete]
+           ,[IsOfficiallyAssigned]
+           ,[RecordCreatedAt]
+           ,[LastChangedOn]
+           ,[Application]
+           ,[Modification]
+           ,[Operator]
+           ,[Organisation]
+           ,[Reason]
+           ,[EventDataAsXml]
+           ,[SyndicationItemCreatedAt]
+           ,[StreetNamePersistentLocalId]
+		   ,[FeedPosition])
+SELECT *, Position FROM [AddressRegistryLegacy].AddressSyndication;
+GO
+
+CREATE NONCLUSTERED INDEX [IX_AddressSyndication_AddressId_2] ON [AddressRegistryLegacy].[AddressSyndication_2]
+(
+	[AddressId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+
+CREATE NONCLUSTERED INDEX [IX_AddressSyndication_PersistentLocalId_2] ON [AddressRegistryLegacy].[AddressSyndication_2]
+(
+	[PersistentLocalId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+
+CREATE COLUMNSTORE INDEX [CI_AddressSyndication_FeedPosition] ON [AddressRegistryLegacy].[AddressSyndication_2] ([FeedPosition]);
+GO
+
+CREATE INDEX [IX_AddressSyndication_Position_2] ON [AddressRegistryLegacy].[AddressSyndication_2] ([Position]);
+GO
---
+++ src/AddressRegistry.Projections.Legacy/Migrations/20250211123736_AddFeedPosition_2.sql
@@ -0,0 +1,57 @@
+﻿SET IDENTITY_INSERT AddressRegistryLegacy.AddressSyndication_2 ON;
+GO
+
+INSERT INTO [AddressRegistryLegacy].[AddressSyndication_2]
+([Position]
+  ,[AddressId]
+  ,[PersistentLocalId]
+  ,[ChangeType]
+  ,[StreetNameId]
+  ,[PostalCode]
+  ,[HouseNumber]
+  ,[BoxNumber]
+  ,[PointPosition]
+  ,[PositionMethod]
+  ,[PositionSpecification]
+  ,[Status]
+  ,[IsComplete]
+  ,[IsOfficiallyAssigned]
+  ,[RecordCreatedAt]
+  ,[LastChangedOn]
+  ,[Application]
+  ,[Modification]
+  ,[Operator]
+  ,[Organisation]
+  ,[Reason]
+  ,[EventDataAsXml]
+  ,[SyndicationItemCreatedAt]
+  ,[StreetNamePersistentLocalId]
+  ,[FeedPosition])
+SELECT *, Position FROM [AddressRegistryLegacy].AddressSyndication
+WHERE Position > (SELECT MAX(Position) FROM [AddressRegistryLegacy].[AddressSyndication_2]);
+
+BEGIN TRAN
+GO
+
+DECLARE @maxId BIGINT = (SELECT MAX(Position) FROM AddressRegistryLegacy.AddressSyndication);
+DBCC CHECKIDENT ('AddressRegistryLegacy.AddressSyndication_2', RESEED, @maxId);
+GO
+
+SET IDENTITY_INSERT AddressRegistryLegacy.AddressSyndication_2 OFF;
+GO
+
+DROP TABLE [AddressRegistryLegacy].[AddressSyndication];
+GO
+
+EXEC sp_rename 'AddressRegistryLegacy.AddressSyndication_2', 'AddressSyndication';
+EXEC sp_rename N'AddressRegistryLegacy.AddressSyndication.IX_AddressSyndication_AddressId_2', N'IX_AddressSyndication_AddressId', N'INDEX';
+EXEC sp_rename N'AddressRegistryLegacy.AddressSyndication.IX_AddressSyndication_PersistentLocalId_2', N'IX_AddressSyndication_PersistentLocalId', N'INDEX';
+EXEC sp_rename N'AddressRegistryLegacy.AddressSyndication.IX_AddressSyndication_Position_2', N'IX_AddressSyndication_Position', N'INDEX';
+EXEC sp_rename N'AddressRegistryLegacy.AddressSyndication.PK_AddressSyndication_2', N'PK_AddressSyndication', N'INDEX';
+
+INSERT INTO [AddressRegistryLegacy].[__EFMigrationsHistoryLegacy] ([MigrationId], [ProductVersion])
+VALUES (N'20250211123736_AddFeedPosition', N'8.0.3');
+GO
+
+COMMIT;
+GO

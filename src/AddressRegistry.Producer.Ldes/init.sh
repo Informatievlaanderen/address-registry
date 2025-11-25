58131fe0c6eb1c76d90fe0c95a093e6a5ebd66ed
ArneD <dumarey.arne@gmail.com>
2025-11-25 16:53:57 +0100

    feat: remove ldes GAWR-7170

--- src/AddressRegistry.Producer.Ldes/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-CONTAINERID=$(curl -s http://169.254.170.2/v2/metadata | jq -r ".Containers[] | select(.Labels[\"com.amazonaws.ecs.container-name\"] | startswith(\"basisregisters-\") and endswith(\"-producer.ldes\")) | .DockerId")
-
-sed -i "s/REPLACE_CONTAINERID/$CONTAINERID/g" appsettings.json
-
-./AddressRegistry.Producer.Ldes

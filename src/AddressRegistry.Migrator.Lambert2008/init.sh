50f8ef9525a9bb42e27cd12d87edf54d133a785e
Arne Dumarey <dumarey.arne+github@pm.me>
2026-09-02 09:01:19 +0200

    fix(migrator): correct containerid in init.sh

--- src/AddressRegistry.Migrator.Lambert2008/init.sh
+++ src/AddressRegistry.Migrator.Lambert2008/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CONTAINERID=$(curl -s http://169.254.170.2/v2/metadata | jq -r ".Containers[] | select(.Labels[\"com.amazonaws.ecs.container-name\"] | startswith(\"basisregisters-\") and endswith(\"-migrator-lambert2008\")) | .DockerId")
+CONTAINERID=$(curl -s http://169.254.170.2/v2/metadata | jq -r ".Containers[] | select(.Labels[\"com.amazonaws.ecs.container-name\"] | startswith(\"basisregisters-\") and endswith(\"-migrator-lambert\")) | .DockerId")
 
 sed -i "s/REPLACE_CONTAINERID/$CONTAINERID/g" appsettings.json
 
